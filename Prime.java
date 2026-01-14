ca22a5c265a11c25a1c00cd320aa83839111816c
sushant-bit <sushantadhikari944@gmail.com>
2026-01-14 09:40:35 +0545

    first-java-program file created

---
+++ Prime.java
@@ -0,0 +1,14 @@
+public class Prime{
+    public static void main(String[] args){
+        int num=21;
+        int c=2;
+        while(c<num){
+            if(num%c==0){
+                System.out.println("Not a prime number");
+            }
+            
+        }
+        
+        System.out.println("Prime number");
+    }
+}

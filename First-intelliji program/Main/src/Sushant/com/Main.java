8e71fcb84920a3cea120451803ff525a6d341151
sushant-bit <sushantadhikari944@gmail.com>
2026-01-15 14:46:24 +0545

    Update Main.java

--- First-intelliji program/Main/src/Sushant/com/Main.java
+++ First-intelliji program/Main/src/Sushant/com/Main.java
@@ -1,9 +1,13 @@
 package Sushant.com;
 
-; //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input=new Scanner(System.in);
+        System.out.println(input.nextLine());
+
+
     }
 }

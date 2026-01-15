cfef8959a14c19076fa10eb68a4b6de8b89d2a88
sushant-bit <sushantadhikari944@gmail.com>
2026-01-15 22:30:53 +0545

    Solving assignment problems

---
+++ Assignment/Practice.java
@@ -0,0 +1,52 @@
+
+import java.util.*;
+public class Practice{
+    public static void main(String[] args) {
+
+        //Write a program to print whether a number is even or odd, also take input from the user.
+
+        Scanner scanner = new Scanner(System.in);
+        // System.out.println("Enter a number: ");
+        // int number = scanner.nextInt();
+
+        //  if(number<1){
+        //     System.out.println("Error: Number Cannot Be in Negative.");
+        // }
+
+        //  else if(number%2==0){
+        //     System.out.println(number +" is an even number");
+        // }
+      
+
+        // else{
+        //     System.out.println(number+" is an odd number");
+        // }
+        
+        //Take name as input and print a greeting message for that particular name.
+
+        // System.out.println("Enter your name: ");
+        // String naam = scanner.nextLine();
+        // System.out.println("Hi " + naam + " , hope youre doing well.");
+
+
+        //Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+
+        // System.out.println("Enter principal: ");
+        // float p = scanner.nextFloat();
+        // System.out.println("Enter time: ");
+        // float t = scanner.nextFloat();
+        // System.out.println("Enter Rate: ");
+        // float r = scanner.nextFloat();
+        // float simpleInterest = (p*t*r)/100;
+
+        // System.out.println("Simple interest is: "+simpleInterest);
+
+        
+
+
+
+        
+        
+
+    }
+}

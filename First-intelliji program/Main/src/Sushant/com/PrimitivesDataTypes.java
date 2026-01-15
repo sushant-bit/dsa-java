273458ac2aba856b9e591f157c5110c1936e139f
sushant-bit <sushantadhikari944@gmail.com>
2026-01-15 15:06:06 +0545

    Create PrimitiveDataTypes.java files
    Learned primitive data types.

---
+++ First-intelliji program/Main/src/Sushant/com/PrimitivesDataTypes.java
@@ -0,0 +1,13 @@
+package Sushant.com;
+
+public class PrimitivesDataTypes {
+    public static void main(String[] args) {
+        int integers=4;
+        char character='S';
+        float pi=3.14f;
+        double largeDecimalNumbers =234234.23243242;
+        long largeIntegersValues=232432542344342L;
+        boolean check =true;
+         
+    }
+}

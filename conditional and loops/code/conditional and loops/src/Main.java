d6c3613afa0b34b6254339ea89bf1d14575d1b70
sushant-bit <sushantadhikari944@gmail.com>
2026-01-16 19:49:43 +0545

    create conditional and loops folder
    understanding the concept of loops and conditional statement.

---
+++ conditional and loops/code/conditional and loops/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int salary = 24000;
+        if(salary>20000){
+            salary+=2000;
+            System.out.println(salary);
+        }
+        else {
+            System.out.println(salary);
+        }
+    }
+}

7b44dacde6b8db96fc62278c7f288e656c1e316d
sushant-bit <sushantadhikari944@gmail.com>
2026-01-16 20:32:48 +0545

    find greates among three using conditional statement

---
+++ conditional and loops/code/conditional and loops/src/Largest.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Largest {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a Frist Number: ");
+        int a= input.nextInt();
+        System.out.println("Enter a Second Number: ");
+        int b= input.nextInt();
+        System.out.println("Enter a Third Number: ");
+        int c= input.nextInt();
+
+        // find the largest of the three numbers
+
+        int max=a;
+        if(b>max){
+            max=b;
+        }
+        if(c>max){
+            max=c;
+        }
+
+        System.out.println(max);
+    }
+}

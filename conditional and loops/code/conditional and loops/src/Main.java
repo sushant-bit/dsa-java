8c2974297008514e0417955904c7d82f3f1dc9b9
sushant-bit <sushantadhikari944@gmail.com>
2026-01-16 19:50:53 +0545

    checing condition

--- conditional and loops/code/conditional and loops/src/Main.java
+++ conditional and loops/code/conditional and loops/src/Main.java
@@ -2,13 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int salary = 24000;
+        int salary = 19000;
         if(salary>20000){
             salary+=2000;
             System.out.println(salary);
         }
         else {
-            System.out.println(salary);
+            System.out.println(salary+1000);
         }
     }
 }

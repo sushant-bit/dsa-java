7de219167de9513a9f99d35ae01c733676dbc6ed
sushant-bit <sushantadhikari944@gmail.com>
2026-01-16 21:52:25 +0545

    update

--- conditional and loops/code/conditional and loops/src/Calculator.java
+++ conditional and loops/code/conditional and loops/src/Calculator.java
@@ -4,6 +4,7 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         //Take input from the user till user does not press S or s
+        int ans =0;
         while(true) {
 //            Take the operator as input
             char s = in.next().trim().charAt(0);
@@ -11,8 +12,39 @@ public class Calculator {
                 //input two numnber
                 int num1= in.nextInt();
                 int num2 = in.nextInt();
-                
+
+                if(s=='+'){
+                    ans=num1+num2;
+                }
+
+                if(s=='-'){
+                    ans=num1-num2;
+                }
+
+                if(s=='*'){
+                    ans=num1*num2;
+                }
+
+                if(s=='/'){
+                    if (num2!=0){
+                    ans=num1/num2;}
+                    else {
+                        System.out.println("Error: num2 cannot be zero");
+                    }
+                }
+
+                if(s=='%'){
+                    ans=num1%num2;
+                }
+
+
+            } else if (s == 'x' || s=='X') {
+                break;
+            }
+            else {
+                System.out.println("Invalid Operation!!!!!!!");
             }
         }
+        System.out.println(ans);
     }
 }

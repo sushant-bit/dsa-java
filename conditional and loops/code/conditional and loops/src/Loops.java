7c956164ac4fe112293897f5690f849801bdfafd
sushant-bit <sushantadhikari944@gmail.com>
2026-01-16 20:08:09 +0545

    trying while loop

--- conditional and loops/code/conditional and loops/src/Loops.java
+++ conditional and loops/code/conditional and loops/src/Loops.java
@@ -4,8 +4,14 @@ public class Loops {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int num= input.nextInt();
-        for (int i=1;i<=num;i++){
-            System.out.println(i);
+//        for (int i=1;i<=num;i++){
+//            System.out.println(i);
+//        }
+        int n=1;
+
+        while(n<=num){
+            System.out.println(n);
+            n++;
         }
     }
 }

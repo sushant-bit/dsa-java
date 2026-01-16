a5392c624c972dc5ea347a575f1258dd017c254c
sushant-bit <sushantadhikari944@gmail.com>
2026-01-16 21:46:40 +0545

    Adding condition

--- conditional and loops/code/conditional and loops/src/Calculator.java
+++ conditional and loops/code/conditional and loops/src/Calculator.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        //Take input from the user till user does not press S or s
+        while(true) {
+//            Take the operator as input
+            char s = in.next().trim().charAt(0);
+            if(s == '+' || s == '-' || s=='*'|| s=='/' || s=='%'){
+                //input two numnber
+                int num1= in.nextInt();
+                int num2 = in.nextInt();
+                
+            }
+        }
     }
 }

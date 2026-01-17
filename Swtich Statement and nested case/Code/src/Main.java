3c5409eeed7ca83b1f9d1f6adf66a635bc6ac748
sushant-bit <sushantadhikari944@gmail.com>
2026-01-17 20:21:19 +0545

    Created switch statement and nested loop file

---
+++ Swtich Statement and nested case/Code/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter fruits name: ");
+        String fruits= in.next();
+
+       
+    }
+}

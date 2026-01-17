5d8ee2428cf6e5d6a43acf77e4486b31bff8b1b3
sushant-bit <sushantadhikari944@gmail.com>
2026-01-17 21:16:12 +0545

    End of the Day

--- Swtich Statement and nested case/Code/src/NestedSwitch.java
+++ Swtich Statement and nested case/Code/src/NestedSwitch.java
@@ -1,5 +1,52 @@
+import java.util.Scanner;
+
 public class NestedSwitch {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int employID = sc.nextInt();
+        String Department=sc.next();
+        switch (employID){
+            case 1:
+                System.out.println("Hey Sushant");
+                break;
+            case 2:
+                System.out.println("prashanna");
+                break;
+            case 3:
+                System.out.println("Employ num 3");
+                switch (Department){
+                    case "IT":
+                        System.out.println("IT Department");
+                        break;
+                    case "Management":
+                        System.out.println("Management Department");
+                        break;
+                    default:
+                        System.out.println("No department entered");
+                }
+                break;
+
+            default:
+                System.out.println("Enter correct employ ID");
+
+        }
+
+
+        // better way to write
+//        switch (employID) {
+//            case 1 -> System.out.println("Hey Sushant");
+//            case 2 -> System.out.println("prashanna");
+//            case 3 -> {
+//                System.out.println("Employ num 3");
+//                switch (Department) {
+//                    case "IT" -> System.out.println("IT Department");
+//                    case "Management" -> System.out.println("Management Department");
+//                    default -> System.out.println("No department entered");
+//                }
+//            }
+//            default -> System.out.println("Enter correct employ ID");
+//        }
+
+
     }
 }

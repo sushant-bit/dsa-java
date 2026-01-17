13df9ccc2e0aca379263982c13844cdd410588a7
sushant-bit <sushantadhikari944@gmail.com>
2026-01-17 09:39:50 +0545

    Update area of circle
    write code to find area of circle by taking radius from the user.

--- conditional and loops/code/conditional and loops/src/Practice/AreaofCircle.java
+++ conditional and loops/code/conditional and loops/src/Practice/AreaofCircle.java
@@ -1,4 +1,14 @@
 package Practice;
 
+import java.util.Scanner;
+
 public class AreaofCircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a radius: ");
+        int radius = sc.nextInt();
+        double pi = 3.141592653589793;
+        double area = pi*(radius*radius);
+        System.out.print("Area of the circle is: "+area);
+    }
 }

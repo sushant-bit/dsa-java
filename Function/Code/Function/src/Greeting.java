8e400d6bafa249694e899091419ca0ada3d2bc86
sushant-bit <sushantadhikari944@gmail.com>
2026-01-18 19:36:49 +0545

    Adding new technique

--- Function/Code/Function/src/Greeting.java
+++ Function/Code/Function/src/Greeting.java
@@ -1,16 +1,22 @@
+import java.util.Scanner;
+
 public class Greeting {
     public static void main(String[] args) {
 //greeting();
-String susu= Mygreet("Pratik Gautam");
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Daalo apna naam aur paao respect: ");
+        String naam = sc.next();
+        String susu= Mygreet(naam);
         System.out.println(susu);
     }
-static String Mygreet(String name){
+    static String Mygreet(String name){
         String Pranam="Pranam "+name;
         return Pranam;
 
-}
+    }
 
     static void greeting(String greet){
         System.out.println("Namaste");
     }
-}
+
+    }

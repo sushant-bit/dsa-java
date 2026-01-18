4f1b6aff315fdd5aa982f47d6e96496d8d6b59d2
sushant-bit <sushantadhikari944@gmail.com>
2026-01-18 22:44:51 +0545

    practice

--- Function/Code/Function/src/VarArgs.java
+++ Function/Code/Function/src/VarArgs.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class VarArgs {
     public static void main(String[] args) {
-        
+//    func("Sushant","Sabita","Prashanna","Pratik","Isha");
+mixy(1,2,"SUSHANT");
+    }
+
+
+    static void mixy(int a,int b,String ...v){
+        System.out.println(Arrays.toString(v));
+    }
+    static void func(String ...s){
+        System.out.println(Arrays.toString(s));
     }
 }

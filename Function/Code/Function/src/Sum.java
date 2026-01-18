32f65ce360ced62704aaee0a149eb0e17e51b265
sushant-bit <sushantadhikari944@gmail.com>
2026-01-18 19:00:47 +0545

    Created sum function

--- Function/Code/Function/src/Sum.java
+++ Function/Code/Function/src/Sum.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Sum {
     public static void main(String[] args) {
-        
+       sum();
     }
+
+    static void sum(){
+        Scanner in = new Scanner(System.in);
+        int n1,n2,sum;
+        System.out.print("Enter first number :");
+        n1=in.nextInt();
+        System.out.print("Enter second number :");
+        n2=in.nextInt();
+        sum=n1+n2;
+        System.out.println("Sum = " +sum);
+    }
+
+
 }

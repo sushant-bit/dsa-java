d99977210ba994ea1298c87d66cf12dcc8fa3ac8
sushant-bit <sushantadhikari944@gmail.com>
2026-01-18 19:23:25 +0545

    using arguments

--- Function/Code/Function/src/StringExample.java
+++ Function/Code/Function/src/StringExample.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class StringExample {
+    public static void main(String[] args) {
+      String nama=greet();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number to print how many time greeting: ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            System.out.println(i +" = " +nama);
+        }
+    }
+
+    static String greet(){
+        String greeting="Namaste K cha";
+        return greeting;
+    }
 }
--- Function/Code/Function/src/Sum.java
+++ Function/Code/Function/src/Sum.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-       int ans =sum2();
+//       int ans =sum2();
+//        System.out.println(ans);
+        int ans =sum3(20,30,60);
         System.out.println(ans);
+    }
+
+    // pass the value of numbers when you are calling the method in main()
+
+    static int sum3(int a,int b,int c){
+        int sum=a+b+c;
+        return sum;
     }
 // return the value
     static int sum2(){

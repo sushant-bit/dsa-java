f8fdfa38a90cc4217f5196fd80fd636a6f1d6603
sushant-bit <sushantadhikari944@gmail.com>
2026-01-18 19:30:40 +0545

    Updating Greeting.java

--- Function/Code/Function/src/Greeting.java
+++ Function/Code/Function/src/Greeting.java
@@ -1,9 +1,16 @@
 public class Greeting {
     public static void main(String[] args) {
-greeting();
+//greeting();
+String susu= Mygreet("Pratik Gautam");
+        System.out.println(susu);
     }
+static String Mygreet(String name){
+        String Pranam="Pranam "+name;
+        return Pranam;
 
-    static void greeting(){
+}
+
+    static void greeting(String greet){
         System.out.println("Namaste");
     }
 }

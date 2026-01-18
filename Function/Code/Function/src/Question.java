3f46caab370a35af1aff55a7b52cee77c55075be
sushant-bit <sushantadhikari944@gmail.com>
2026-01-18 23:54:39 +0545

    The end Function
    In this section i have learn a lot about the uses of function.

--- Function/Code/Function/src/Question.java
+++ Function/Code/Function/src/Question.java
@@ -2,16 +2,37 @@ import java.util.Scanner;
 
 public class Question {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        boolean ans = isPrime(n);
-        System.out.println(ans);
-        if(ans==true){
-            System.out.println("Oho balle balle ye toh prime hai!!!");
+//        Scanner sc = new Scanner(System.in);
+//        int n=sc.nextInt();
+//        boolean ans = armstr(n);
+//        System.out.println(ans);
+//        if(ans==true){
+//            System.out.println("Oho balle balle ye toh prime hai!!!");
+//        }
+//        else {
+//            System.out.println("Ooopssss.....! Not a PrimeNumber");
+//        }
+
+        for (int i = 100; i <1000 ; i++) {
+            if (armstr(i)){
+                System.out.println(i+ " = Armstrong Number ");
+            }
         }
-        else {
-            System.out.println("Ooopssss.....! Not a PrimeNumber");
+    }
+
+
+    static boolean armstr(int n){
+        int ori=n;
+        int sum=0;
+
+        while(n>0){
+        int rem =n%10;
+        n=n/10;
+        sum= sum+rem*rem*rem;
+
         }
+
+        return sum == ori;
     }
 
     static boolean isPrime (int n){

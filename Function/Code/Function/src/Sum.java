9c7f37a34c644300ea5edaf4071e0e8390a6fb64
sushant-bit <sushantadhikari944@gmail.com>
2026-01-18 19:08:50 +0545

    return the value of sum

--- Function/Code/Function/src/Sum.java
+++ Function/Code/Function/src/Sum.java
@@ -2,7 +2,19 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-       sum();
+       int ans =sum2();
+        System.out.println(ans);
+    }
+// return the value
+    static int sum2(){
+        Scanner in = new Scanner(System.in);
+        int n1,n2,sum;
+        System.out.print("Enter first number :");
+        n1=in.nextInt();
+        System.out.print("Enter second number :");
+        n2=in.nextInt();
+        sum=n1+n2;
+        return sum;
     }
 
     static void sum(){

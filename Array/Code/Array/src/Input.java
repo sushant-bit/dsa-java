7a06543af9829fe32e6e378cf87a1d63f633a192
sushant-bit <sushantadhikari944@gmail.com>
2026-01-19 21:46:31 +0545

    Accessing value of array using loops

--- Array/Code/Array/src/Input.java
+++ Array/Code/Array/src/Input.java
@@ -1,5 +1,14 @@
 public class Input {
     public static void main(String[] args) {
-        
+         int[] arr = new int[5];
+         arr[0]=23;
+         arr[1]=2334;
+        System.out.println(arr[1]);
+
+
+        // input using for loops
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(i);
+        }
     }
 }

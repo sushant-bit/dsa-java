ce6cfa601506c16b297056cfa71a169cdc26d04e
sushant-bit <sushantadhikari944@gmail.com>
2026-01-19 21:04:11 +0545

    updating

--- Array/Code/Array/src/Main.java
+++ Array/Code/Array/src/Main.java
@@ -1,15 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //syntax
+        //datatype[] variableName= new datatype[size];
+        // Store 5 roll numbers
+       int[] roll= new int[10];
+        // store a person name
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }

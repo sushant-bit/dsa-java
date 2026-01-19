315230aa0a4edb857a6b2f5b65e79450ce590369
sushant-bit <sushantadhikari944@gmail.com>
2026-01-19 21:23:39 +0545

    update

--- Array/Code/Array/src/Main.java
+++ Array/Code/Array/src/Main.java
@@ -3,8 +3,14 @@ public class Main {
         //syntax
         //datatype[] variableName= new datatype[size];
         // Store 5 roll numbers
-       int[] roll= new int[10];
-        // store a person name
+//       int[] roll= new int[5];
+//        System.out.println(roll);
+//       // or directly you can write
+//        int[] rols={1,23,23,23,44};
+//        // store a person name
+
+        int[] roll; //declaration on an array. roll is getting define in the stack
+        roll = new int[5]; // initialization actually here object is being created in the memory/heap memory
 
     }
 }

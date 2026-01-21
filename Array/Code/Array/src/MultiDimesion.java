51b39a53cadf56bfc994eadd34eea1e0b9ff0dfc
sushant-bit <sushantadhikari944@gmail.com>
2026-01-21 21:11:58 +0545

    Update MultiDimesion.java

--- Array/Code/Array/src/MultiDimesion.java
+++ Array/Code/Array/src/MultiDimesion.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+
 public class MultiDimesion {
     public static void main(String[] args) {
-        
+        /*
+        1 2 3
+        4 5 6
+        7 8 9
+         */
+
+
+//        int[][] matrixarr= new int[3][];
+//         int[][] matrixar={
+//                 {1,2,3},
+//                 {3,2,1},
+//                 {1,2,3}
+//         };
+            int [][] arr = {
+                    {1,2,3},
+                    {4,5},
+                    {6,7,8,9}
+            };
+        System.out.println(Arrays.toString(arr[0]));
     }
 }

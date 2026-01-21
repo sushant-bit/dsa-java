b7aec4ff9c6c962bbe9d250e5439f5a43be9fe95
sushant-bit <sushantadhikari944@gmail.com>
2026-01-21 20:58:40 +0545

    Update PassingInFunction.java

--- Array/Code/Array/src/PassingInFunction.java
+++ Array/Code/Array/src/PassingInFunction.java
@@ -1,2 +1,15 @@
+import java.util.Arrays;
+
 public class PassingInFunction {
+    public static void main(String[] args) {
+        int[] num= {3,4,5,7};
+        System.out.println(Arrays.toString(num));
+        change(num);
+        System.out.println(Arrays.toString(num));
+
+    }
+
+    static void change(int[] arr){
+        arr[0]=99;
+    }
 }

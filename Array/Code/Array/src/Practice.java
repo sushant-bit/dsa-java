1f6fb456eaadd38a704ef9c3b47eb30fa3c28c87
sushant-bit <sushantadhikari944@gmail.com>
2026-01-22 21:13:59 +0545

    Update Practice.java

--- Array/Code/Array/src/Practice.java
+++ Array/Code/Array/src/Practice.java
@@ -17,7 +17,7 @@ public class Practice {
                 newArray[row][col]= sc.nextInt();
             }
         }
-
+    //step to show output
 
         for (int row = 0; row < newArray.length; row++) {
             System.out.println(Arrays.toString(newArray[row]));

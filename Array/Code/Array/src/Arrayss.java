cc03fe561b2264fb3e2f95c24dd763b9c0307acc
sushant-bit <sushantadhikari944@gmail.com>
2026-01-22 21:40:19 +0545

    checking it with while loop

--- Array/Code/Array/src/Arrayss.java
+++ Array/Code/Array/src/Arrayss.java
@@ -14,10 +14,14 @@ public class Arrayss {
         //this process is for taking input from the user
 
         // this first loop is used to loop in row section
-        for (int row = 0; row < matrix.length; row++) {
-            //this is loop is for column section
-            for (int col = 0; col < matrix[row].length; col++) {
-                matrix[row][col]=sc.nextInt();
+        {
+            int row = 0;
+            while (row < matrix.length) {
+                //this is loop is for column section
+                for (int col = 0; col < matrix[row].length; col++) {
+                    matrix[row][col]=sc.nextInt();
+                }
+                row++;
             }
         }
 

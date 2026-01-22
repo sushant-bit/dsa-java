10c4d98070ae2a68477c55448a6fe99b30a61751
sushant-bit <sushantadhikari944@gmail.com>
2026-01-22 21:32:37 +0545

    Update Arrayss.java

--- Array/Code/Array/src/Arrayss.java
+++ Array/Code/Array/src/Arrayss.java
@@ -23,6 +23,13 @@ public class Arrayss {
 
         //this process is for printing the output
 
-        
+        for (int row = 0; row < matrix.length; row++) {
+            //this is loop is for column section
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.print(matrix[row][col]);
+            }
+            System.out.println();
+        }
+
     }
 }

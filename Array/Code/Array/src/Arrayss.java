2b04184623c0bead7860a229eb582bb7f3ac2c6c
sushant-bit <sushantadhikari944@gmail.com>
2026-01-22 21:24:46 +0545

    Update Arrayss.java

--- Array/Code/Array/src/Arrayss.java
+++ Array/Code/Array/src/Arrayss.java
@@ -1,13 +1,14 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Arrayss {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a row: ");
+        System.out.print("Enter a row: ");
         int rows = sc.nextInt();
         System.out.print("Enter a column: ");
         int column = sc.nextInt();
         int[][] matrix = new int[rows][column];
-        System.out.println(matrix);
+        System.out.print(Arrays.toString(matrix));
     }
 }

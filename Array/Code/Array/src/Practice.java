9502363c82f8adca246488d77cc21dcc3e6250b2
sushant-bit <sushantadhikari944@gmail.com>
2026-01-22 21:16:13 +0545

    creating function

--- Array/Code/Array/src/Practice.java
+++ Array/Code/Array/src/Practice.java
@@ -2,6 +2,10 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Practice {
+
+    static int sum(int a, int b){
+        return a+b;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a row: ");

953205161585a626065c46c46e63aac85428f231
sushant-bit <sushantadhikari944@gmail.com>
2026-01-22 21:12:41 +0545

    Create Practice.java

---
+++ Array/Code/Array/src/Practice.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a row: ");
+        int rows=sc.nextInt();
+        System.out.print("Enter a column: ");
+        int column= sc.nextInt();
+        int[][] newArray = new int[rows][column];
+
+        for (int row = 0; row < newArray.length; row++) {
+            for (int col = 0; col < newArray[row].length; col++) {
+                newArray[row][col]= sc.nextInt();
+            }
+        }
+
+
+        for (int row = 0; row < newArray.length; row++) {
+            System.out.println(Arrays.toString(newArray[row]));
+        }
+
+    }
+}

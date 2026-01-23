7c94ff6d431f175bc5fcfad98e775ace50a30866
sushant-bit <sushantadhikari944@gmail.com>
2026-01-23 20:18:40 +0545

    Created ArrayList

--- Array/Code/Array/src/ColumnNoFixed.java
+++ Array/Code/Array/src/ColumnNoFixed.java
@@ -8,10 +8,8 @@ public class ColumnNoFixed {
         for (int row = 0; row < arr.length; row++) {
             for (int col = 0; col < arr[row].length; col++) {
                 System.out.print(arr[row][col]+" ");
-
             }
             System.out.println();
-
         }
     }
 }

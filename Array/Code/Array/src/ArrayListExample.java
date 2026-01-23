c36180f7052e271d05ae15db43eed930d02d1510
sushant-bit <sushantadhikari944@gmail.com>
2026-01-23 20:21:17 +0545

    Update ArrayListExample.java

--- Array/Code/Array/src/ArrayListExample.java
+++ Array/Code/Array/src/ArrayListExample.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+
 public class ArrayListExample {
     public static void main(String[] args) {
         //syntax
+        ArrayList<Integer> list = new ArrayList<>();
     }
 }

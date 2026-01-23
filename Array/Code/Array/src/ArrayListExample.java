d2c4bd65329aa32f97694c39344e6b9bf19afe3e
sushant-bit <sushantadhikari944@gmail.com>
2026-01-23 20:44:58 +0545

    Checking user number is entered or not by the user

--- Array/Code/Array/src/ArrayListExample.java
+++ Array/Code/Array/src/ArrayListExample.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 public class ArrayListExample {
     public static void main(String[] args) {
 
+        long sushant_phone=9812178081L;
+
         Scanner in = new Scanner(System.in);
         //syntax
-        ArrayList<Integer> list = new ArrayList<>(5);
+        ArrayList<Long> list = new ArrayList<>(5);
 //        list.add(56);
 //        list.add(256);
 //        list.add(256);
@@ -23,11 +25,11 @@ public class ArrayListExample {
         //input
 
         for (int i = 0; i < 5; i++) {
-        list.add(in.nextInt());
+        list.add(in.nextLong());
         }
 
-        if (list.contains(28)){
-            System.out.print("yes yes yes user enter 28");
+        if (list.contains(sushant_phone)){
+            System.out.println("Here is the phone number of sushant: "+ sushant_phone);
         }
 
         //output

b7d86d5410d2ba077145e9b064a9e1c093f7392a
sushant-bit <sushantadhikari944@gmail.com>
2026-01-23 20:48:19 +0545

    Update ArrayListExample.java

--- Array/Code/Array/src/ArrayListExample.java
+++ Array/Code/Array/src/ArrayListExample.java
@@ -32,7 +32,7 @@ public class ArrayListExample {
 
         //get item in any index
         for (int i = 0; i < 5; i++) {
-            System.out.println(list.);
+            System.out.print(list.get(i)+ " ");//pass index here
         }
 
 

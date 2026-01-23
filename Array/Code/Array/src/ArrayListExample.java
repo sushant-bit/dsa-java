36d7e78764639cf56c990834c4587abbb157ce28
sushant-bit <sushantadhikari944@gmail.com>
2026-01-23 20:41:17 +0545

    Update ArrayListExample.java

--- Array/Code/Array/src/ArrayListExample.java
+++ Array/Code/Array/src/ArrayListExample.java
@@ -7,17 +7,17 @@ public class ArrayListExample {
         Scanner in = new Scanner(System.in);
         //syntax
         ArrayList<Integer> list = new ArrayList<>(5);
-        list.add(56);
-        list.add(256);
-        list.add(256);
-        list.add(23256);
-        list.add(5632);
-        list.add(5623);
-//        System.out.println(list.contains(562));
-        System.out.println(list);
-        list.set(0,99);
-        list.remove(4);
-        System.out.println(list);
+//        list.add(56);
+//        list.add(256);
+//        list.add(256);
+//        list.add(23256);
+//        list.add(5632);
+//        list.add(5623);
+////        System.out.println(list.contains(562));
+//        System.out.println(list);
+//        list.set(0,99);
+//        list.remove(4);
+//        System.out.println(list);
         
         
         //input
@@ -25,6 +25,13 @@ public class ArrayListExample {
         for (int i = 0; i < 5; i++) {
         list.add(in.nextInt());
         }
+
+        if (list.contains(28)){
+            System.out.print("yes yes yes user enter 28");
+        }
+
+        //output
+        System.out.println(list);
 
 
     }

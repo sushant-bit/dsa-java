07fdfa07c38857a7acbbbbbac21d06a9db0309f1
sushant-bit <sushantadhikari944@gmail.com>
2026-01-23 21:19:48 +0545

    Update MultiDimensionArrayList.java

--- Array/Code/Array/src/MultiDimensionArrayList.java
+++ Array/Code/Array/src/MultiDimensionArrayList.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MultiDimensionArrayList {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         ArrayList<ArrayList<Integer>> mdarr= new ArrayList<>();
         //initialization
         for (int i = 0; i < 3; i++) {
@@ -9,5 +11,12 @@ public class MultiDimensionArrayList {
         }
 
         //add elements
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                mdarr.get(i).add(in.nextInt());
+            }
+        }
+
+        System.out.print(mdarr);
     }
 }

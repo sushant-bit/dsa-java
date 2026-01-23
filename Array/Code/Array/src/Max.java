d5d0d3409a45bff17a30b38c82f199cfdd318df8
sushant-bit <sushantadhikari944@gmail.com>
2026-01-23 21:50:11 +0545

    Update Max.java

--- Array/Code/Array/src/Max.java
+++ Array/Code/Array/src/Max.java
@@ -1,5 +1,15 @@
 public class Max {
     public static void main(String[] args) {
-        
+        int[] arr = {1,2,3,4,5,6};
+        System.out.println(max(arr));
+    }
+
+    static int main(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int max = arr[0];
+            if (max>arr[i]){
+                max=arr[i];
+            }
+        }
     }
 }

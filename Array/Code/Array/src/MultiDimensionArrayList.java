d40671f65aeb5bd202c9be141c036e084c2e4511
sushant-bit <sushantadhikari944@gmail.com>
2026-01-23 21:14:53 +0545

    Update MultiDimensionArrayList.java

--- Array/Code/Array/src/MultiDimensionArrayList.java
+++ Array/Code/Array/src/MultiDimensionArrayList.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+
 public class MultiDimensionArrayList {
     public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> mdarr= new ArrayList<>();
         
     }
 }

0544c5e8be52e667632890cf17b22387929ae042
sushant-bit <sushantadhikari944@gmail.com>
2026-01-23 20:37:24 +0545

    Update ArrayListExample.java

--- Array/Code/Array/src/ArrayListExample.java
+++ Array/Code/Array/src/ArrayListExample.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArrayListExample {
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
         //syntax
         ArrayList<Integer> list = new ArrayList<>(5);
         list.add(56);
@@ -10,12 +13,18 @@ public class ArrayListExample {
         list.add(23256);
         list.add(5632);
         list.add(5623);
-        System.out.println(list.contains(562));
+//        System.out.println(list.contains(562));
         System.out.println(list);
         list.set(0,99);
         list.remove(4);
-        System.out.println(list.toArray().length);
         System.out.println(list);
+        
+        
+        //input
+
+        for (int i = 0; i < 5; i++) {
+        list.add(in.nextInt());
+        }
 
 
     }

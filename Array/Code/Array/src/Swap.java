2e048a8ce4170eaecb00d11998846733aef982b3
sushant-bit <sushantadhikari944@gmail.com>
2026-01-23 21:43:02 +0545

    Update Swap.java

--- Array/Code/Array/src/Swap.java
+++ Array/Code/Array/src/Swap.java
@@ -1,8 +1,22 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Swap {
     public static void main(String[] args) {
-        int[] arr ={1,2,3,4,5,6,};
+        int[] arr ={1,2,3,4,5,6,7,8,9};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Choose the two index to swap their value: ");
+        int in1=sc.nextInt();
+        int in2= sc.nextInt();
+        swap(arr,in1,in2);
+        System.out.println(Arrays.toString(arr));
+        
+    }
+
+    static void swap(int[] arr,int indexOne,int indextwo){
+        int swapping = arr[indexOne];
+        arr[indexOne]=arr[indextwo];
+        arr[indextwo]=swapping;
     }
 
 

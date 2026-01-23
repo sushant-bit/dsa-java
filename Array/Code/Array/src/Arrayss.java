3ac7b35d8c39006220b47b818d7a13302397f0e4
sushant-bit <sushantadhikari944@gmail.com>
2026-01-23 20:09:55 +0545

    Created a new class

--- Array/Code/Array/src/Arrayss.java
+++ Array/Code/Array/src/Arrayss.java
@@ -28,5 +28,6 @@ public class Arrayss {
             System.out.println(Arrays.toString(matrix[row]));
         }
 
+
     }
 }
---
+++ Array/Code/Array/src/ColumnNoFixed.java
@@ -0,0 +1,5 @@
+public class ColumnNoFixed {
+    public static void main(String[] args) {
+        
+    }
+}

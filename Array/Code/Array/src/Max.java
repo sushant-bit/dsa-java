5665c492acef029bb5bb1e4c07b73122fb2292f8
sushant-bit <sushantadhikari944@gmail.com>
2026-01-23 21:57:07 +0545

    Update Max.java

--- Array/Code/Array/src/Max.java
+++ Array/Code/Array/src/Max.java
@@ -1,7 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Max {
     public static void main(String[] args) {
-        int[] arr = new int[2];
-       
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter how much value you want to add in the array: ");
+        int value= sc.nextInt();
+        int[] arr = new int[value];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(Arrays.toString(arr));
         System.out.println(maxval(arr));
     }
 //imagine that arrray is not empty

a6df75d6482c021dc6693593220d22fccedebf33
sushant-bit <sushantadhikari944@gmail.com>
2026-01-24 20:40:30 +0545

    Update Main.java

--- Linear Search/code/Linear Search/src/Main.java
+++ Linear Search/code/Linear Search/src/Main.java
@@ -2,7 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-
+    int [] arr ={18,19,20,32,77,80};
     }
 }

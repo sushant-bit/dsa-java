64a54d3ed855f56bbfa81f211f822c6dc1e56c67
sushant-bit <sushantadhikari944@gmail.com>
2026-01-24 20:41:11 +0545

    Update Main.java

--- Linear Search/code/Linear Search/src/Main.java
+++ Linear Search/code/Linear Search/src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    int [] arr ={18,19,20,32,77,80};
+    int [] arr ={18,19,20,32,77,80,14};
+    //find whether 14 exist or not
     }
 }

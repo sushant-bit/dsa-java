52b1a843e90b582910c7a32edea65f951660a62b
sushant-bit <sushantadhikari944@gmail.com>
2026-01-24 21:10:14 +0545

    Update Main.java

--- Linear Search/code/Linear Search/src/Main.java
+++ Linear Search/code/Linear Search/src/Main.java
@@ -1,18 +1,28 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    int [] arr ={18,19,20,32,77,80,14};
+    int [] arr = new int[1000];
     //find whether 14 exist in array or not
 
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i]==14){
+//                System.out.println("Yes this array Contain 14");
+//            }
+//            else {
+//                System.out.println("Opps not 14 in the array");
+//                break;
+//            }
+//        }
+
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i]==14){
-                System.out.println("Yes this array Contain 14");
-            }
-            else {
-                System.out.println("Opps not 14 in the array");
-                break;
-            }
+            arr[i]=i;
         }
+
+       
+
+        System.out.print(Arrays.toString(arr));
     }
 }

ad1636e079c4a0f0d78bccb4e8af1ecca567b7c2
sushant-bit <sushantadhikari944@gmail.com>
2026-01-24 20:45:43 +0545

    Update Main.java

--- Linear Search/code/Linear Search/src/Main.java
+++ Linear Search/code/Linear Search/src/Main.java
@@ -3,6 +3,16 @@
 public class Main {
     public static void main(String[] args) {
     int [] arr ={18,19,20,32,77,80,14};
-    //find whether 14 exist or not
+    //find whether 14 exist in array or not
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]==14){
+                System.out.println("Yes this array Contain 14");
+            }
+            else {
+                System.out.println("Opps not 14 in the array");
+                break;
+            }
+        }
     }
 }

d8ebcca35713644e7ad2d4e2e9623b66580018aa
sushant-bit <sushantadhikari944@gmail.com>
2026-01-26 22:56:21 +0545

    solve the Leetcode problem 1295

--- Linear Search/code/Linear Search/src/PracticeLeedcode.java
+++ Linear Search/code/Linear Search/src/PracticeLeedcode.java
@@ -1,18 +1,25 @@
+// Leetcode problem 1295
+
 public class PracticeLeedcode {
     public static void main(String[] args) {
-        int[] nums={23,432412,3243,323,34,42,30,54};
-        int result=evennumber(nums);
+        int[] arr={12,232,54,45,65,543,565543};
+        int result = Findnumber(arr);
         System.out.println(result);
     }
 
-    static int evennumber(int[] nums){
-        int count =0;
+    static int Findnumber(int[] nums){
+        int count=0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i]%2==0){
+            int digits=0;
+            while(nums[i]>0){
+                digits++;
+                nums[i] /=10;
+            }
+
+            if (digits%2==0){
                 count++;
             }
-        }
-        return count;
+        } return count;
     }
 
 }

c1d0bb7113c258250608e4a12872353e25bb1e1e
sushant-bit <sushantadhikari944@gmail.com>
2026-01-26 21:48:54 +0545

    Update SearchIn2DArray.java

--- Linear Search/code/Linear Search/src/SearchIn2DArray.java
+++ Linear Search/code/Linear Search/src/SearchIn2DArray.java
@@ -1,2 +1,22 @@
+import java.util.Arrays;
+
 public class SearchIn2DArray {
+    public static void main(String[] args) {
+        int[][] arr= {
+                {1230,23,231,2123},
+                {233,324,56654,2335,55},
+                {232,342,432,2,42,4324,324,3423,2}
+        };
+        int target=42;
+        System.out.println(search(arr,target));
+
+    }
+
+    static int search(int[][] arr, int target){
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                
+            }
+        }
+    }
 }

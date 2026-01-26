c851037ff32d6b81fd4ae453a14cf94f89ccc8a4
sushant-bit <sushantadhikari944@gmail.com>
2026-01-26 19:56:00 +0545

    Update SearchInString.java

--- Linear Search/code/Linear Search/src/SearchInString.java
+++ Linear Search/code/Linear Search/src/SearchInString.java
@@ -1,9 +1,14 @@
 public class SearchInString {
     public static void main(String[] args) {
-        String name = "Sushant"
+        String name = "Sushant";
+        char target='u';
+        System.out.println(search(name, target));
     }
 
-    static boolean search(){
+    static boolean search(String str, char tar){
+        if (str.length()==0){
+            return false;
+        }
 
     }
 }

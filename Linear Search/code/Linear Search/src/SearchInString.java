9d329a2cf619b781338fb5be38cea2871947ad1e
sushant-bit <sushantadhikari944@gmail.com>
2026-01-26 20:11:14 +0545

    Update SearchInString.java

--- Linear Search/code/Linear Search/src/SearchInString.java
+++ Linear Search/code/Linear Search/src/SearchInString.java
@@ -1,9 +1,27 @@
+import java.util.Arrays;
+
 public class SearchInString {
     public static void main(String[] args) {
         String name = "Sushant";
         char target='u';
         System.out.println(name.length());
         System.out.println(search(name, target));
+        System.out.println(search1(name,target));
+        System.out.println(Arrays.toString(name.toCharArray()));
+    }
+
+    static boolean search1(String str, char tar){
+        if (str.length()==0){
+            return false;
+        }
+        for (char ch:str.toCharArray()){
+            if (ch==tar){
+                return true;
+            }
+
+        }
+
+        return false;
     }
 
     static boolean search(String str, char tar){

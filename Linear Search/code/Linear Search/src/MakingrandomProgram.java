fd83c7d2fe5af55cc869b24024fca2be3c48b959
sushant-bit <sushantadhikari944@gmail.com>
2026-01-27 20:21:09 +0545

    making program which ask user to ente rrow and column in 2D array

--- Linear Search/code/Linear Search/src/MakingrandomProgram.java
+++ Linear Search/code/Linear Search/src/MakingrandomProgram.java
@@ -1,3 +1,12 @@
+import java.util.Scanner;
+
 public class MakingrandomProgram {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a row how many you want in 2D array: ");
+        int rows= sc.nextInt();
+        System.out.print("Enter a column how many you want in 2D array: ");
+        int column = sc.nextInt();
+        int[][] arr = new int[rows][column];
+    }
 }

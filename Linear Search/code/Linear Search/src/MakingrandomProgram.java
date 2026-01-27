1f6a92c500aa4c8d4acd7abb291f7b4132dc7800
sushant-bit <sushantadhikari944@gmail.com>
2026-01-27 20:38:43 +0545

    Update MakingrandomProgram.java

--- Linear Search/code/Linear Search/src/MakingrandomProgram.java
+++ Linear Search/code/Linear Search/src/MakingrandomProgram.java
@@ -3,19 +3,56 @@ import java.util.Scanner;
 
 public class MakingrandomProgram {
     public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter a row how many you want in 2D array: ");
+//        int rows= sc.nextInt();
+//        System.out.print("Enter a column how many you want in 2D array: ");
+//        int column = sc.nextInt();
+//        int[][] arr = new int[rows][column];
+//
+//        //asking user input
+//        for (int row = 0; row < rows; row++) {
+//            for (int col = 0; col < column; col++) {
+//                arr[row][col]= sc.nextInt();
+//            }
+//        }
+//
+//        //for output
+//        for (int row = 0; row < rows; row++) {
+//            for (int col = 0; col < column; col++) {
+//                System.out.print(arr[row][col]+" ");
+//            }
+//
+//            System.out.println();
+//        }
+//
+//        System.out.println(arr);
+
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a row how many you want in 2D array: ");
-        int rows= sc.nextInt();
-        System.out.print("Enter a column how many you want in 2D array: ");
+
+        System.out.print("Enter rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter columns: ");
         int column = sc.nextInt();
+
         int[][] arr = new int[rows][column];
 
-        //asking user input
+        System.out.println("Enter elements:");
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < column; col++) {
-                arr[row][col]= sc.nextInt();
+                arr[row][col] = sc.nextInt();
             }
         }
+
+        System.out.println("2D Array:");
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < column; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+
 
     }
 

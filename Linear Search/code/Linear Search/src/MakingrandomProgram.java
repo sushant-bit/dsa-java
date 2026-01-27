3f6fd02da7a852d8808f5b5bd239ec7f15d083a9
sushant-bit <sushantadhikari944@gmail.com>
2026-01-27 21:08:59 +0545

    Update MakingrandomProgram.java

--- Linear Search/code/Linear Search/src/MakingrandomProgram.java
+++ Linear Search/code/Linear Search/src/MakingrandomProgram.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class MakingrandomProgram {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a row how many you want in 2D array: ");
+        System.out.print("Enter a row: ");
         int rows= sc.nextInt();
-        System.out.print("Enter a column how many you want in 2D array: ");
+        System.out.print("Enter a column: ");
         int column = sc.nextInt();
         int[][] arr = new int[rows][column];
 
@@ -24,7 +24,6 @@ public class MakingrandomProgram {
             }
             System.out.println();
         }
-        
 
         System.out.println("Richest person have: "+ maxWealth(arr));
 

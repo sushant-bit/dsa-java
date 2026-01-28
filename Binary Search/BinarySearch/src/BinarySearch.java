df3749c12ad17ea79ba86d9cf39c55d9d38312e6
sushant-bit <sushantadhikari944@gmail.com>
2026-01-28 21:12:41 +0545

    Update BinarySearch.java

--- Binary Search/BinarySearch/src/BinarySearch.java
+++ Binary Search/BinarySearch/src/BinarySearch.java
@@ -1,5 +1,17 @@
 public class BinarySearch {
     public static void main(String[] args) {
-       
+
+    }
+
+
+    //return the index
+    //return -1 if target doesnot exist
+    static int binarySearch(int[] arr,int target){
+        int start=0;
+        int end = arr.length-1;
+        while(start<=end){
+            //find the middle element
+            
+        }
     }
 }

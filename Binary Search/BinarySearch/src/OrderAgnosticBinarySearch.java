087dcabef68a166de3798ec64ef9562a8fa34b92
sushant-bit <sushantadhikari944@gmail.com>
2026-01-28 21:57:53 +0545

    Update OrderAgnosticBinarySearch.java

--- Binary Search/BinarySearch/src/OrderAgnosticBinarySearch.java
+++ Binary Search/BinarySearch/src/OrderAgnosticBinarySearch.java
@@ -1,2 +1,36 @@
 public class OrderAgnosticBinarySearch {
+    public static void main(String[] args) {
+
+    }
+
+    static int orderAgnosticBinarySearch(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        //find the array is sorted in ascending or descending
+        boolean isAsc=arr[start]<arr[end] ;
+        //same thing
+//        if (arr[start]<arr[end]){
+//            isAsc=true;
+//        }
+//        else {
+//            isAsc=false;
+//        }
+        
+        while (start <= end) {
+            //find the middle element
+//            int mid=(start+end)/2;// if start and end have exceed value than the integer then it will give an erro.
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                //ans found
+                return mid;
+            }
+        }
+        return -1;
+    }
 }

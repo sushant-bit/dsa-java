3200591e74ec4d21d56a56cf72a449b8003506cc
sushant-bit <sushantadhikari944@gmail.com>
2026-01-28 22:01:39 +0545

    Making binarysearch according to ascending and descending

--- Binary Search/BinarySearch/src/OrderAgnosticBinarySearch.java
+++ Binary Search/BinarySearch/src/OrderAgnosticBinarySearch.java
@@ -1,5 +1,6 @@
 public class OrderAgnosticBinarySearch {
     public static void main(String[] args) {
+        int
 
     }
 
@@ -16,20 +17,32 @@ public class OrderAgnosticBinarySearch {
 //        else {
 //            isAsc=false;
 //        }
-        
+
         while (start <= end) {
             //find the middle element
 //            int mid=(start+end)/2;// if start and end have exceed value than the integer then it will give an erro.
             int mid = start + (end - start) / 2;
 
-            if (target < arr[mid]) {
-                end = mid - 1;
-            } else if (target > arr[mid]) {
-                start = mid + 1;
-            } else {
-                //ans found
+            if (arr[mid]==target){
                 return mid;
             }
+
+            if (isAsc){
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            else {
+                if (target < arr[mid]) {
+                    start = mid + 1;
+
+                } else{
+                    end = mid - 1;
+                }
+            }
+
         }
         return -1;
     }

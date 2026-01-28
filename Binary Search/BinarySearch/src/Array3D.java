ab1c5b415aaefc054c17098b9685c01d7350f12d
sushant-bit <sushantadhikari944@gmail.com>
2026-01-28 14:13:30 +0545

    Update Array3D
    Add the function which ask user for input in 3D Array and give output.

--- Binary Search/BinarySearch/src/Array3D.java
+++ Binary Search/BinarySearch/src/Array3D.java
@@ -1,28 +1,48 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Array3D {
     public static void main(String[] args) {
-        int [][][] arr={
-                {
-                        {12,23,34,44,3},
-                        {234,344,3543,4}
-                },
-                {
-                        {43,44,5,45,4,54,3},
-                        {3,34,34334,34,34,43}
-                },
-                {
-                        {65,655,33455},
-                        {343,8,87,8,5,9,4}
-                }
-        };
-        int target = 6;
-        System.out.println(array3Ds1(arr,target));
-
+        Scanner sc = new Scanner(System.in);
+//        int [][][] arr={
+//                {
+//                        {12,23,34,44,3},
+//                        {234,344,3543,4}
+//                },
+//                {
+//                        {43,44,5,45,4,54,3},
+//                        {3,34,34334,34,34,43}
+//                },
+//                {
+//                        {65,655,33455},
+//                        {343,8,87,8,5,9,4}
+//                }
+//        };
+//        int target = 34;
+//        System.out.println(array3Ds1(arr,target));
+        System.out.print("Enter layer: ");
+        int layer= sc.nextInt();
+        System.out.print("Enter row: ");
+        int row = sc.nextInt();
+        System.out.print("Enter column: ");
+        int col =sc.nextInt();
+        int[][][] arr = new int[layer][row][col];
+        array3Ds(arr);
     }
 
     static void array3Ds(int[][][] arr){
+        Scanner sc = new Scanner(System.in);
+        //taking input
+        for (int layer = 0; layer < arr.length; layer++) {
+            for (int row = 0; row < arr[layer].length; row++) {
+                for (int col = 0; col < arr[layer][row].length; col++) {
+                    arr[layer][row][col]=sc.nextInt();
+                }
+            }
+        }
+
+        //output
         for (int layer = 0; layer < arr.length; layer++) {
             System.out.println("Layer "+layer);
             for (int row = 0; row < arr[layer].length; row++) {

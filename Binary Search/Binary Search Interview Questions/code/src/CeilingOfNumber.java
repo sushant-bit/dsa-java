a3f50d02909b5d2e35a3090d7c76442afe022380
sushant-bit <sushantadhikari944@gmail.com>
2026-01-29 22:48:06 +0545

    Update CeilingOfNumber.java

--- Binary Search/Binary Search Interview Questions/code/src/CeilingOfNumber.java
+++ Binary Search/Binary Search Interview Questions/code/src/CeilingOfNumber.java
@@ -2,7 +2,7 @@
 public class CeilingOfNumber {
     public static void main(String[] args) {
         int [] arr ={2,3,5,9,14,16,18};
-        System.out.println(ceilingofnum(arr,5));
+        System.out.println(ceilingofnum(arr,6));
     }
     static  int ceilingofnum(int[] arr, int target){
         int start=0;

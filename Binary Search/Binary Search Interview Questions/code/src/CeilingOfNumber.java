143a4eff9813ccffb6a5c06d8dcc583e22ac9568
sushant-bit <sushantadhikari944@gmail.com>
2026-01-29 22:40:01 +0545

    Update CeilingOfNumber.java

--- Binary Search/Binary Search Interview Questions/code/src/CeilingOfNumber.java
+++ Binary Search/Binary Search Interview Questions/code/src/CeilingOfNumber.java
@@ -9,13 +9,10 @@ public class CeilingOfNumber {
         int end=arr.length-1;
         while(start<=end){
             int mid=start+(end-start)/2;
-                for (int i = 0; i < arr.length; i++) {
-
-            }
             if (arr[mid]<target){
-                end=mid-1;
-            } else if (arr[mid]>target) {
                 start=mid+1;
+            } else if (arr[mid]>target) {
+                end=mid-1;
             }
             else {
                 return mid;

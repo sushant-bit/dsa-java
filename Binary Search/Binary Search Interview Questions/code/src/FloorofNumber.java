18096855489f8b7b0c070ed48cf2ab9a7e497dc9
sushant-bit <sushantadhikari944@gmail.com>
2026-01-29 23:03:19 +0545

    Update FloorofNumber.java

--- Binary Search/Binary Search Interview Questions/code/src/FloorofNumber.java
+++ Binary Search/Binary Search Interview Questions/code/src/FloorofNumber.java
@@ -1,5 +1,24 @@
 public class FloorofNumber {
     public static void main(String[] args) {
-        
+        int[] arr ={2,3,5,9,14,16,18};
+        System.out.println(floorOfnum(arr,17));
+
+    }
+
+    static int floorOfnum(int[] arr, int target){
+        int start=0;
+        int end= arr.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if (target>arr[mid]){
+                start=mid+1;
+            } else if (target<arr[mid]) {
+                end=mid-1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return end;
     }
 }

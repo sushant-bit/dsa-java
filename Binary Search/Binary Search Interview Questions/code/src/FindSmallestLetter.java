b27a439f4b44dcfe7a5a30d196ebddea44bcf992
sushant-bit <sushantadhikari944@gmail.com>
2026-01-30 00:03:23 +0545

    Update FindSmallestLetter.java

--- Binary Search/Binary Search Interview Questions/code/src/FindSmallestLetter.java
+++ Binary Search/Binary Search Interview Questions/code/src/FindSmallestLetter.java
@@ -4,21 +4,21 @@
 //Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
 public class FindSmallestLetter {
     public static void main(String[] args) {
-        int [] arr ={'c','f','j'};
-        System.out.println(ceilingofnum(arr,159));
+        char [] arr ={'c','f','j'};
+        System.out.println(smallestletter(arr,'f'));
     }
-    static  int smallestletter(char[] letters, char target){
+    static  char smallestletter(char[] letters, char target){
         int start=0;
         int end=letters.length-1;
         while(start<=end){
             int mid=start+(end-start)/2;
-            if (letters[mid]<target){
+            if (letters[mid]<=target){
                 start=mid+1;
             } else{
                 end=mid-1;
             }
 
         }
-        return start; // when the condition is violated so it will return the start. which is the closest greatest number of the target
+        return letters[start%letters.length];
     }
 }

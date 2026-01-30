f5c5322da11bdb2388b289f38e364ced7bffa635
sushant-bit <sushantadhikari944@gmail.com>
2026-01-30 20:09:11 +0545

    Update FindFirstandLastPosition.java

--- Binary Search/Binary Search Interview Questions/code/src/FindFirstandLastPosition.java
+++ Binary Search/Binary Search Interview Questions/code/src/FindFirstandLastPosition.java
@@ -1,2 +1,10 @@
+//leetcode problem number 34
 public class FindFirstandLastPosition {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        
+    }
 }

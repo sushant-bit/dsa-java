80bf66c4bc9a6536aa2eb1c816b836be07473afb
sushant-bit <sushantadhikari944@gmail.com>
2026-01-30 20:21:20 +0545

    Update FindFirstandLastPosition.java

--- Binary Search/Binary Search Interview Questions/code/src/FindFirstandLastPosition.java
+++ Binary Search/Binary Search Interview Questions/code/src/FindFirstandLastPosition.java
@@ -5,6 +5,19 @@ public class FindFirstandLastPosition {
     }
 
     public int[] searchRange(int[] nums, int target) {
-        
+        int start =0;
+        int end=nums.length;
+        while(start<=end){
+            int mid= start+(end-start)/2;
+            if(target<nums[mid]){
+                end=mid-1;
+            } else if (target>nums[mid]){
+                start=mid+1;
+            }
+            else{
+                return ;
+            }
+        }
+        return nums[-1,-1];
     }
 }

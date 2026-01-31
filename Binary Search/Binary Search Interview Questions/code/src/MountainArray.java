4350c45b0ceb6684a66a5d16e3a51d1678964f34
sushant-bit <sushantadhikari944@gmail.com>
2026-01-31 21:51:21 +0545

    Update MountainArray.java

--- Binary Search/Binary Search Interview Questions/code/src/MountainArray.java
+++ Binary Search/Binary Search Interview Questions/code/src/MountainArray.java
@@ -1,3 +1,22 @@
+//852. Peak Index in a Mountain Array
 public class MountainArray {
-    
+    public static void main(String[] args) {
+
+    }
+
+    public int peakIndexInMountainArray(int[] arr) {
+            int start=0;
+            int end = arr.length-1;
+            while (start<end){
+                int mid = start+(end-start)/2;
+                if (arr[mid]>arr[mid+1]){
+                    //now youre in decreasing part of array this may be answer but look at left
+                    //this is why end is not = mid-1
+                    end=mid;
+                }
+                else {
+                    //youre in ascending part of an array
+                }
+            }
+    }
 }

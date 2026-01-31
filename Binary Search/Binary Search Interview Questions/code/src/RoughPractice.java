2dac471b967fff1c9372149212cf25ea1280acd2
sushant-bit <sushantadhikari944@gmail.com>
2026-01-31 12:30:32 +0545

    Update RoughPractice.java

--- Binary Search/Binary Search Interview Questions/code/src/RoughPractice.java
+++ Binary Search/Binary Search Interview Questions/code/src/RoughPractice.java
@@ -1,5 +1,11 @@
 public class RoughPractice {
     public static void main(String[] args) {
-        
+
     }
+
+    static int[] firstandlast(int[] arr, int target){
+        int[] ans={-1,-1};
+    }
+
+    static int[] finfing index
 }

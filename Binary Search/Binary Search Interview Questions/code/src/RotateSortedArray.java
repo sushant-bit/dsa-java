a04fc32350c21546418faada3b8e13dd48f5b55c
sushant-bit <sushantadhikari944@gmail.com>
2026-02-02 22:06:03 +0545

    Update RotateSortedArray.java

--- Binary Search/Binary Search Interview Questions/code/src/RotateSortedArray.java
+++ Binary Search/Binary Search Interview Questions/code/src/RotateSortedArray.java
@@ -11,8 +11,13 @@ public class RotateSortedArray {
             if (mid<end && arr[mid]>arr[mid+1]){
                 return mid;
             }
-            if (mid>start && arr[mid]<arr[mid-1]){
+            else if (mid>start && arr[mid]<arr[mid-1]){
                 return mid-1;
+            } else if (arr[mid] <= arr[start]) {
+                end=mid-1;
+            }
+            else{
+                    
             }
 
 

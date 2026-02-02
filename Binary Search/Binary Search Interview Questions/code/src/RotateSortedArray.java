6999c91cabc67b24397f874844570ed26ae85f3d
sushant-bit <sushantadhikari944@gmail.com>
2026-02-02 20:36:55 +0545

    Update RotateSortedArray.java

--- Binary Search/Binary Search Interview Questions/code/src/RotateSortedArray.java
+++ Binary Search/Binary Search Interview Questions/code/src/RotateSortedArray.java
@@ -1,13 +1,21 @@
 public class RotateSortedArray {
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,5,6,7,8};
-        System.out.println(searchInRotateArray(arr,1));
+
     }
     static int findPivot(int[] arr){
         int start=0;
         int end = arr.length-1;
         while (start<=end){
             int mid = start+(end-start)/2;
+            //four cases
+            if (arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            if (arr[mid]<arr[mid+1]){
+                return mid-1;
+            }
+
+
         }
     }
 

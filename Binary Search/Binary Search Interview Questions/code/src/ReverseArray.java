e6a0cec20b380f5e909daa8f3c1c3972607aff54
sushant-bit <sushantadhikari944@gmail.com>
2026-02-03 22:13:08 +0545

    Update ReverseArray.java

--- Binary Search/Binary Search Interview Questions/code/src/ReverseArray.java
+++ Binary Search/Binary Search Interview Questions/code/src/ReverseArray.java
@@ -6,5 +6,6 @@ public class ReverseArray {
     static int[] reverseArray(int [] arr){
         int start=0; //this is the first index of array
         int end = arr.length-1; //this is the last index of array
+        
     }
 }

3748a439985de0452e38e70735347974ca121833
sushant-bit <sushantadhikari944@gmail.com>
2026-02-03 21:24:04 +0545

    Update FindCountofRotatedArray.java

--- Binary Search/Binary Search Interview Questions/code/src/FindCountofRotatedArray.java
+++ Binary Search/Binary Search Interview Questions/code/src/FindCountofRotatedArray.java
@@ -1,2 +1,11 @@
 public class FindCountofRotatedArray {
+    public static void main(String[] args) {
+int[] arr={2,4,5,7,0,1,3,6};
+        System.out.println(countRotations(arr));
+    }
+
+    private static int countRotations(int[] arr) {
+        
+    }
+
 }

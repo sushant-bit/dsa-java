2be4d87c798a747b7728e152dae23acd9fec2eb4
sushant-bit <sushantadhikari944@gmail.com>
2026-02-03 22:12:41 +0545

    Updating the code and initializing the starting index and ending index of an array

--- Binary Search/Binary Search Interview Questions/code/src/ReverseArray.java
+++ Binary Search/Binary Search Interview Questions/code/src/ReverseArray.java
@@ -1,2 +1,10 @@
 public class ReverseArray {
+    public static void main(String[] args) {
+
+    }
+
+    static int[] reverseArray(int [] arr){
+        int start=0; //this is the first index of array
+        int end = arr.length-1; //this is the last index of array
+    }
 }

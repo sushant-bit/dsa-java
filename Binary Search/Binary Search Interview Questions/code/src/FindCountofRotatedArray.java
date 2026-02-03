27f27f69dbd425a2cd4fe3b53d521463fe4c57fc
sushant-bit <sushantadhikari944@gmail.com>
2026-02-03 21:25:33 +0545

    Update FindCountofRotatedArray.java

--- Binary Search/Binary Search Interview Questions/code/src/FindCountofRotatedArray.java
+++ Binary Search/Binary Search Interview Questions/code/src/FindCountofRotatedArray.java
@@ -7,37 +7,7 @@ int[] arr={2,4,5,7,0,1,3,6};
     private static int countRotations(int[] arr) {
 
     }
-
-    static int binarySearchPivot(int[] arr,int target){
-        int pivot=findPivot(arr);
-        //if you didnot find the pivot, it means the array is not rotated
-        if (pivot==-1){
-            //just do normal binarysearch
-            return binarySearch(arr,target,0,arr.length-1);
-        }
-        else if(arr[pivot]==target){
-            return pivot;
-        }
-        else if(target>=arr[0]){
-            return binarySearch(arr,target,0,pivot-1);
-        }
-
-        return binarySearch(arr,target,pivot+1,arr.length-1);
-    }
-    static int binarySearch(int[] arr, int target, int start,int end){
-        while (start<=end){
-            int mid= start+(end-start)/2;
-            if (target>arr[mid]){
-                start=mid+1;
-            } else if (target<arr[mid]) {
-                end=mid-1;
-            }
-            else {
-                return mid;
-            }
-        }
-        return -1;
-    }
+    
     static int findPivot(int[] arr){
         int start=0;
         int end = arr.length-1;

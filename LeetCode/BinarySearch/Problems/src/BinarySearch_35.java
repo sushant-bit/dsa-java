7586f52a1c749634ebb7d771508a4e72dc2a281e
sushant-bit <sushantadhikari944@gmail.com>
2026-02-04 22:14:25 +0545

    Update BinarySearch_35.java

--- LeetCode/BinarySearch/Problems/src/BinarySearch_35.java
+++ LeetCode/BinarySearch/Problems/src/BinarySearch_35.java
@@ -1,6 +1,7 @@
 public class BinarySearch_35 {
     public static void main(String[] args) {
-
+        int[] arr={1,3,5,9};
+        System.out.println(searchInsert(arr,2));
     }
 
     static int searchInsert(int[] arr,int target){

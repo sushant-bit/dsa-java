df0fd31b9f27a2fc5e214ac20d5d25e8a5ae3f87
sushant-bit <sushantadhikari944@gmail.com>
2026-02-04 21:37:40 +0545

    Run

--- Binary Search/Binary Search Interview Questions/code/src/SplitArraysLargestSum.java
+++ Binary Search/Binary Search Interview Questions/code/src/SplitArraysLargestSum.java
@@ -1,7 +1,9 @@
 //problem no 410 leetcode #Hard-Type
 public class SplitArraysLargestSum {
     public static void main(String[] args){
-
+        int[] arr={2,4,8,10,15,17,21,33};
+        int target=2;
+        System.out.println(binarySearch(arr,target));
     }
     static int binarySearch(int arr[],int target){
         int start=0;

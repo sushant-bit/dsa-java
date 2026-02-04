5a92ab22dbfbf015839ee2c8a72e6cc6d51e5335
sushant-bit <sushantadhikari944@gmail.com>
2026-02-04 22:00:33 +0545

    feat: add binary search problem 704 solution

---
+++ LeetCode/BinarySearch/Problems/src/BinarySearch_704.java
@@ -0,0 +1,27 @@
+// LeetCode 704 - Binary Search
+// Time: O(log n)
+// Space: O(1)
+public class BinarySearch_704 {
+    public static void main(String[] args) {
+
+
+    }
+
+    public int search(int[] nums, int target) {
+        int start=0;
+        int end =nums.length-1;
+
+        while (start<=end){
+            int mid = start+(end-start)/2;
+            if (target>nums[mid]){
+                start=mid+1;
+            } else if (target<nums[mid]) {
+                end=mid-1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}

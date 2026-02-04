78748c8b5a09b282251f0e3306f5ee43e60a4a1f
sushant-bit <sushantadhikari944@gmail.com>
2026-02-04 21:35:23 +0545

    Created binarySearch

--- Binary Search/Binary Search Interview Questions/code/src/SplitArraysLargestSum.java
+++ Binary Search/Binary Search Interview Questions/code/src/SplitArraysLargestSum.java
@@ -4,6 +4,19 @@ public class SplitArraysLargestSum {
 
     }
     static int binarySearch(int arr[],int target){
-        
+        int start=0;
+        int end = arr.length-1;
+        while (start<=end){
+            int mid = start+(end-start)/2;
+            if (target>arr[mid]){
+                start=mid+1;
+            } else if (target<arr[mid]) {
+                end=mid-1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }

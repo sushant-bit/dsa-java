a966c6d0521946cde0e4e5e31b05eb9dda8bde69
sushant-bit <sushantadhikari944@gmail.com>
2026-02-05 22:35:04 +0545

    Solved

--- LeetCode/BinarySearch/Problems/src/Problem_367.java
+++ LeetCode/BinarySearch/Problems/src/Problem_367.java
@@ -3,5 +3,24 @@ public class Problem_367 {
     public static void main(String[] args) {
 
     }
-    
+    public boolean isPerfectSquare(int num) {
+        if(num<2){
+            return true;
+        }
+        int start=1;
+        int end = num;
+        while (start<end){
+            int mid = start+(end-start)/2;
+            int square=mid*mid;
+            if (square==num){
+                return true;
+            }else if (square<num){
+                start=mid+1;
+            }
+            else {
+                end=mid-1;
+            }
+        }
+        return false;
+    }
 }

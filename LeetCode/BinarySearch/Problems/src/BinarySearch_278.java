05c4ba9937f0b5a25e2f90c404a0e0edf8f484f7
sushant-bit <sushantadhikari944@gmail.com>
2026-02-05 22:04:11 +0545

    Problem solved

--- LeetCode/BinarySearch/Problems/src/BinarySearch_278.java
+++ LeetCode/BinarySearch/Problems/src/BinarySearch_278.java
@@ -2,7 +2,30 @@ public class BinarySearch_278 {
     public static void main(String[] args) {
 
     }
-    public int firstBadVersion(int n) {
-        
+
+
+
+    static boolean isBadVersion(int version) {
+            int bad = 4;
+            return version >= bad;
+        }
+
+
+    static int firstBadVersion(int n) {
+        int start=1;
+        int end = n;
+
+        while(start<end){
+            int mid = start+(end-start)/2;
+            if(isBadVersion(mid)){
+                end=mid;
+            }
+            else {
+                start=mid+1;
+            }
+        }
+        return start;
     }
+
+
 }

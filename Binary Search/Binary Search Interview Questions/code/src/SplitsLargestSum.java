271bb7abe995bf4e24b3d3d60bf11ea521c4519f
sushant-bit <sushantadhikari944@gmail.com>
2026-02-06 22:08:28 +0545

    Removing unnecessary code

--- Binary Search/Binary Search Interview Questions/code/src/SplitsLargestSum.java
+++ Binary Search/Binary Search Interview Questions/code/src/SplitsLargestSum.java
@@ -8,24 +8,15 @@ public class SplitsLargestSum {
         int end = 0;
         for (int i = 0; i < nums.length; i++) {
             start=Math.max(start,nums[i]); //this will contain the max item from the array
+            end+=nums[i];
         }
+        while (start<end){
+            //try for the middle as potential answer
+            int mi=start+(end-start)/2;
+
+        }
+        return end;
     }
 
-    static int binarySearch(int[] arr, int target){
-        int start=0;
-        int end = arr.length-1;
-        while (start<=end){
-            int mid = start+(end-start)/2;
-            if (target>arr[mid]){
-                start=mid+1;
-            }
-            else if(target<arr[mid]){
-                end=mid-1;
-            }
-            else{
-                return mid;
-            }
-        }
-        return -1;
-    }
+    
 }

6d2bd89f1c34f6448c968c1f57065a2f5af8aea3
sushant-bit <sushantadhikari944@gmail.com>
2026-02-06 22:17:52 +0545

    Solved hard problem of leetcode

--- Binary Search/Binary Search Interview Questions/code/src/SplitsLargestSum.java
+++ Binary Search/Binary Search Interview Questions/code/src/SplitsLargestSum.java
@@ -1,9 +1,10 @@
 public class SplitsLargestSum {
     public static void main(String[] args) {
-
+        int [] arr= {7,2,5,10,8};
+        System.out.println(splitArray(arr,3));
     }
 
-    public int splitArray(int[] nums, int k) {
+    static int splitArray(int[] nums, int k) {
         int start=0;
         int end = 0;
         for (int i = 0; i < nums.length; i++) {

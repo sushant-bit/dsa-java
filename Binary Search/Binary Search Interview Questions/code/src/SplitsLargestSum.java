afa92eea70ddf41f1ec0f73a25d5d6d6b4dcfa18
sushant-bit <sushantadhikari944@gmail.com>
2026-02-06 22:06:29 +0545

    Updating code

--- Binary Search/Binary Search Interview Questions/code/src/SplitsLargestSum.java
+++ Binary Search/Binary Search Interview Questions/code/src/SplitsLargestSum.java
@@ -4,7 +4,11 @@ public class SplitsLargestSum {
     }
 
     public int splitArray(int[] nums, int k) {
-
+        int start=0;
+        int end = 0;
+        for (int i = 0; i < nums.length; i++) {
+            start=Math.max(start,nums[i]); //this will contain the max item from the array
+        }
     }
 
     static int binarySearch(int[] arr, int target){

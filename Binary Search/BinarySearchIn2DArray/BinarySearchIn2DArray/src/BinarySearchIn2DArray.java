c3d31aaa50b595f89c9639af9d06cc2a8dee54d1
sushant-bit <sushantadhikari944@gmail.com>
2026-02-07 21:16:09 +0545

    c

--- Binary Search/BinarySearchIn2DArray/BinarySearchIn2DArray/src/BinarySearchIn2DArray.java
+++ Binary Search/BinarySearchIn2DArray/BinarySearchIn2DArray/src/BinarySearchIn2DArray.java
@@ -9,6 +9,16 @@ public class BinarySearchIn2DArray {
     static int[] search(int[][] matrix,int target){
         int row=0;
         int col=matrix.length-1;
-        
+        while (row<matrix.length && col>=0){
+            if (matrix[row][col]==target){
+                return new int[]{row,col};
+            } else if (matrix[row][col]<target) {
+                row++;
+            }
+            else {
+                col--;
+            }
+        }
+        return new int[]{-1,-1};
     }
 }

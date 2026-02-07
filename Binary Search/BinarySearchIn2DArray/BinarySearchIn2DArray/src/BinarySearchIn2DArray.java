950edd445e6100497a3374a068f618693621b991
sushant-bit <sushantadhikari944@gmail.com>
2026-02-07 20:55:40 +0545

    Update BinarySearchIn2DArray.java

--- Binary Search/BinarySearchIn2DArray/BinarySearchIn2DArray/src/BinarySearchIn2DArray.java
+++ Binary Search/BinarySearchIn2DArray/BinarySearchIn2DArray/src/BinarySearchIn2DArray.java
@@ -1,6 +1,22 @@
+import java.util.Arrays;
 
 public class BinarySearchIn2DArray {
     public static void main(String[] args) {
-       
+        int[][] arr = {
+                {1,10,12,14},
+                {20,45,50}
+        };
+        System.out.println(Arrays.toString(searchIn2D(arr,20)));
+    }
+
+    static int[] searchIn2D(int[][] arr, int target){
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col]==target){
+                    return new int[]{row,col};
+                }
+            }
+        }
+        return new int[]{-1,-1};
     }
 }

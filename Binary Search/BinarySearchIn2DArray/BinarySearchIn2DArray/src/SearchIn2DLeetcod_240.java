ebae61e2ec498706948b21195a7e2aa3db8b1f19
sushant-bit <sushantadhikari944@gmail.com>
2026-02-07 21:55:25 +0545

    Problem solved

--- Binary Search/BinarySearchIn2DArray/BinarySearchIn2DArray/src/SearchIn2DLeetcod_240.java
+++ Binary Search/BinarySearchIn2DArray/BinarySearchIn2DArray/src/SearchIn2DLeetcod_240.java
@@ -5,10 +5,32 @@
 ////Integers in each column are sorted in ascending from top to bottom.
 public class SearchIn2DLeetcod_240 {
     public static void main(String[] args) {
-
+        int[][] arr={
+                {1,4,7,11,15},
+                {2,5,8,12,19},
+                {3,6,9,16,22},
+                {10,13,14,17,24},
+                {18,21,23,26,30}
+        };
+        System.out.println(searchMatrix(arr,5));
     }
 
-    public boolean searchMatrix(int[][] matrix, int target) {
-
+    static boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length==0 || matrix[0].length==0){
+            return false;
+        }
+        int row=0;
+        int col=matrix[0].length-1;
+        while (row<matrix.length&&col>=0){
+            if (matrix[row][col]==target){
+                return true;
+            } else if (matrix[row][col]>target) {
+                col--;
+            }
+            else {
+                row++;
+            }
+        }
+        return false;
     }
 }

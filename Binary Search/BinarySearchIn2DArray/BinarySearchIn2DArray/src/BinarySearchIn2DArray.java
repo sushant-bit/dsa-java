9ed73a97697382b9c9372c8a1503347ef5ac219c
sushant-bit <sushantadhikari944@gmail.com>
2026-02-07 21:19:03 +0545

    Done

--- Binary Search/BinarySearchIn2DArray/BinarySearchIn2DArray/src/BinarySearchIn2DArray.java
+++ Binary Search/BinarySearchIn2DArray/BinarySearchIn2DArray/src/BinarySearchIn2DArray.java
@@ -2,7 +2,13 @@ import java.util.Arrays;
 
 public class BinarySearchIn2DArray {
     public static void main(String[] args) {
-
+        int[][] arr={
+                {10,20,30,40},
+                {15,25,35,45},
+                {28,29,37,49},
+                {33,34,38,50}
+        };
+        System.out.println(Arrays.toString( search(arr,37)));
     }
 
 

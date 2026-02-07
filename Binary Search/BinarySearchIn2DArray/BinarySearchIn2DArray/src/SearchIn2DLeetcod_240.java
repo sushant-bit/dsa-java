fac658080fc4c3c2d0fadfe9e367c72d643fc396
sushant-bit <sushantadhikari944@gmail.com>
2026-02-07 21:50:52 +0545

    Solving leetcode problem

---
+++ Binary Search/BinarySearchIn2DArray/BinarySearchIn2DArray/src/SearchIn2DLeetcod_240.java
@@ -0,0 +1,14 @@
+////240. Search a 2D Matrix II
+//Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+//
+//Integers in each row are sorted in ascending from left to right.
+////Integers in each column are sorted in ascending from top to bottom.
+public class SearchIn2DLeetcod_240 {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+    }
+}

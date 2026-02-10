f2c72d904243215b983b51641c1f15f815032e98
sushant-bit <sushantadhikari944@gmail.com>
2026-02-10 22:12:01 +0545

    Update SearchInSortedMatrix.java

--- Binary Search/BinarySearchIn2DArray/BinarySearchIn2DArray/src/SearchInSortedMatrix.java
+++ Binary Search/BinarySearchIn2DArray/BinarySearchIn2DArray/src/SearchInSortedMatrix.java
@@ -3,38 +3,12 @@ public class SearchInSortedMatrix {
 
     }
 
-//    search in the row which is provided between the coloumns
-    static int[] binarySearch(int[][] matrix,int row,int cStart,int cEnd,int target){
-        while (cStart<=cEnd){
-            int mid= cStart+(cEnd-cStart)/2;
-            if (matrix[row][mid]==target){
-                return new int[]{row,mid};
-            } else if (target>matrix[row][mid]) {
-                cStart=mid+1;
-            }
-            else {
-                cEnd=mid-1;
-            }
-        }
-        return new int[]{-1,-1};
-    }
+ static int[] SearchIn2DSortedMatrix(int[][] arr, int target){
+        int row=arr.length;
+        int col=arr[0].length;
 
-    static int[] search(int[][] matrix, int target){
-        int row=matrix.length;
-        int col=matrix[0].length;// be aware, matrix may be empty
+        int start=0;
+        int end=(row*col)-1;  // total elements in the 2D matrix -1 to find the last index.
+ }
 
-        if (row==1){
-            return binarySearch(matrix,0,0,col-1,target);
-        }
-
-        int rstart=0;
-        int rend=row-1;
-        int cMid=col/2;
-        //run the loop till 2 rows are remaining
-        while (rstart<(rend-1)){
-
-        }
-
-
-    }
 }

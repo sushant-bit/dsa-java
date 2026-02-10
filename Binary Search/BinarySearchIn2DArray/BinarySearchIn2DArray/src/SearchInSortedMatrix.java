f93a919ff376e7def87c41c1b65bc47de65fe231
sushant-bit <sushantadhikari944@gmail.com>
2026-02-10 22:31:07 +0545

    Solved

--- Binary Search/BinarySearchIn2DArray/BinarySearchIn2DArray/src/SearchInSortedMatrix.java
+++ Binary Search/BinarySearchIn2DArray/BinarySearchIn2DArray/src/SearchInSortedMatrix.java
@@ -9,6 +9,22 @@ public class SearchInSortedMatrix {
 
         int start=0;
         int end=(row*col)-1;  // total elements in the 2D matrix -1 to find the last index.
+
+     while(start<=end){
+         int mid=start+(end-start)/2;
+         int mrow=mid/col;
+         int mcol=mid%col;
+
+         if (arr[mrow][mcol]>target){
+             end=mid-1;
+         } else if (target>arr[mrow][mcol]) {
+             start=mid+1;
+         }
+         else{
+             return new int[]{mrow,mcol};
+         }
+     }
+     return new int[]{-1,-1};
  }
 
 }

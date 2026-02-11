ba949a02dae8f1788421843d7b37ac85100a4af7
sushant-bit <sushantadhikari944@gmail.com>
2026-02-11 22:47:04 +0545

    Solved

--- LeetCode/Random_Solving Question/Code/src/Problem_1480_RunningSum.java
+++ LeetCode/Random_Solving Question/Code/src/Problem_1480_RunningSum.java
@@ -1,2 +1,16 @@
+import java.util.Arrays;
+
 public class Problem_1480_RunningSum {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4};
+        int [] res= runsum(arr);
+        System.out.println(Arrays.toString(res));
+    }
+
+    static int[] runsum(int[]arr){
+        for (int i = 1; i < arr.length; i++) {
+            arr[i]+=arr[i-1];
+        }
+        return arr;
+    }
 }

c3bac39d38dae9f2a452153636046107465bd6bd
sushant-bit <sushantadhikari944@gmail.com>
2026-02-11 20:17:27 +0545

    Addded macbook() function

--- Practicum/Simple_Basic_Project/src/Basic_Billing_System.java
+++ Practicum/Simple_Basic_Project/src/Basic_Billing_System.java
@@ -6,6 +6,7 @@ public class Basic_Billing_System {
         System.out.println("------->Welcome to Eleven Store<-------");
         System.out.println("Enter the Product ID to Add Item to Your Cart");
         System.out.println("---->Product Details<----");
+        int total_bill;
         int key = 1199;
         int mou = 499;
         int st = 450;
@@ -16,8 +17,11 @@ public class Basic_Billing_System {
         System.out.println("103: Stand (Rs " + st + ")");
         System.out.println("104: USB-C (Rs" + us + ")");
         System.out.println("105: PS-5 (Rs" + ps + ")");
-        System.out.println("106: MacBook");
-        System.out.println("107: Mobile Phone");
+        System.out.println("106: MacBook List");
+        System.out.println("107: Mobile Phone List");
+        System.out.println("Enter Product ID You Want to Buy");
+
+
     }
 
     static int mobile(Scanner sc){
@@ -40,32 +44,42 @@ public class Basic_Billing_System {
 
             if(choice == 1){
                 System.out.println(" Added Samsung Galaxy S25 Ultra to Your cart");
+                mobile_total_price += 184999;
             }
             else if(choice == 2){
                 System.out.println(" Added Samsung Galaxy A36 5G to Your cart");
+                mobile_total_price += 54999;
             }
             else if(choice == 3){
                 System.out.println(" Added Apple iPhone 17 to Your cart");
+                mobile_total_price += 197999;
             }
             else if(choice == 4){
                 System.out.println(" Added Xiaomi 15 Ultra to Your cart");
+                mobile_total_price += 149999;
             }
             else if(choice == 5){
                 System.out.println(" Added OnePlus 12 to Your cart");
+                mobile_total_price += 139999;
             }
             else if(choice == 6){
                 System.out.println(" Added Vivo V50 5G to Your cart");
+                mobile_total_price += 67999;
             }
             else if(choice == 7){
                 System.out.println(" Added Realme C67 5G to Your cart");
+                mobile_total_price += 21999;
             }
             else if(choice == 8){
                 System.out.println(" Added Redmi Note 14 5G to Your cart");
+                mobile_total_price += 29999;
             }
             else if(choice == 9){
                 System.out.println(" Added OnePlus Nord CE3 to Your cart");
-            } else if (choice==0) {
-                shopping=false; // this will break the loop and return the total amount.
+                mobile_total_price += 38499;
+            }
+            else if (choice == 0) {
+                shopping = false;
             }
             else{
                 System.out.println("Invalid output");
@@ -74,5 +88,47 @@ public class Basic_Billing_System {
         return mobile_total_price;
 
     }
+    static int macbook(Scanner sc) {
+        int macbook_total_price = 0;
+        boolean shopping = true;
+
+        while (shopping) {
+            System.out.println("Select a MacBook");
+            System.out.println("1: MacBook Air M2 (8/256GB) - Rs. 159999");
+            System.out.println("2: MacBook Air M2 (16/512GB) - Rs. 199999");
+            System.out.println("3: MacBook Pro 14-inch (16/512GB) - Rs. 249999");
+            System.out.println("4: MacBook Pro 16-inch (32/1TB) - Rs. 399999");
+            System.out.println("0: Finish adding MacBooks");
+            System.out.print("Enter choice: ");
+
+            int choice = sc.nextInt();
+
+            if (choice == 1) {
+                System.out.println(" Added MacBook Air M2 (8/256GB) to Your cart");
+                macbook_total_price += 159999;
+            }
+            else if (choice == 2) {
+                System.out.println(" Added MacBook Air M2 (16/512GB) to Your cart");
+                macbook_total_price += 199999;
+            }
+            else if (choice == 3) {
+                System.out.println(" Added MacBook Pro 14-inch (16/512GB) to Your cart");
+                macbook_total_price += 249999;
+            }
+            else if (choice == 4) {
+                System.out.println(" Added MacBook Pro 16-inch (32/1TB) to Your cart");
+                macbook_total_price += 399999;
+            }
+            else if (choice == 0) {
+                shopping = false;
+            }
+            else {
+                System.out.println("Invalid output");
+            }
+        }
+
+        return macbook_total_price;
+    }
+
 
 }

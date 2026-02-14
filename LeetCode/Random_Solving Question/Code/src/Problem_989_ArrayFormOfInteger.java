dcfac21e30dae0acb819fc6c633f7d9dfa6f2f9b
sushant-bit <sushantadhikari944@gmail.com>
2026-02-14 19:42:33 +0545

    Update Problem_989_ArrayFormOfInteger.java

--- LeetCode/Random_Solving Question/Code/src/Problem_989_ArrayFormOfInteger.java
+++ LeetCode/Random_Solving Question/Code/src/Problem_989_ArrayFormOfInteger.java
@@ -1,2 +1,15 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem_989_ArrayFormOfInteger {
+    public static void main(String[] args) {
+
+    }
+
+
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        
+    }
+
 }

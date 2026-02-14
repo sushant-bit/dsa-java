4e34e8b1f526c19fba622bdf4c7246638bf4ec45
sushant-bit <sushantadhikari944@gmail.com>
2026-02-14 19:47:53 +0545

    Solved Problem 989

--- LeetCode/Random_Solving Question/Code/src/Problem_989_ArrayFormOfInteger.java
+++ LeetCode/Random_Solving Question/Code/src/Problem_989_ArrayFormOfInteger.java
@@ -9,7 +9,25 @@ public class Problem_989_ArrayFormOfInteger {
 
 
     public List<Integer> addToArrayForm(int[] num, int k) {
-        
+        List<Integer> ans = new ArrayList<>();
+        int i = num.length - 1;
+
+        while (i >= 0 || k > 0) {
+            if (i >= 0) {
+                k += num[i];
+                i--;
+            }
+            ans.add(k % 10);
+            k /= 10;
+        }
+
+        List<Integer> finalResult = new ArrayList<>();
+        for (int j = ans.size() - 1; j >= 0; j--) {
+            finalResult.add(ans.get(j));
+        }
+
+        return finalResult;
     }
+
 
 }

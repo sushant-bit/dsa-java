14507b358f38dbfcfeb7774f8e589b38b40614d0
sushant-bit <sushantadhikari944@gmail.com>
2026-02-14 19:23:23 +0545

    Solve LeetCode 1512: Number of Good Pairs

---
+++ LeetCode/Random_Solving Question/Code/src/Problem_1512_GoodPairs.java
@@ -0,0 +1,16 @@
+public class Problem_1512_GoodPairs {
+    public static void main(String[] args) {
+
+    }
+    public int numIdenticalPairs(int[] nums) {
+        int count=0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j <nums.length ; j++) {
+                if (nums[i]==nums[j]){
+                    count++;
+                }
+            }
+        }
+return count;
+    }
+}

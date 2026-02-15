8d0f11c2318f6b9fda3f0bc203d7276b89b02872
sushant-bit <sushantadhikari944@gmail.com>
2026-02-15 10:15:42 +0545

    Building logic

--- LeetCode/Random_Solving Question/Code/src/Problem_832_FlippinganImage.java
+++ LeetCode/Random_Solving Question/Code/src/Problem_832_FlippinganImage.java
@@ -4,8 +4,15 @@ public class Problem_832_FlippinganImage {
     }
 
     public int[][] flipAndInvertImage(int[][] image) {
-        for (int i = 0; i < i<image.length; i++) {
+        for (int i = 0; i < image.length; i++) {
             int start=0;
+            int end=image[i].length-1;
+            while (start<=end){
+                int temp=image[i][start];
+                image[i][start]=image[i][end];
+                image[i][end]=temp;
+            }
+
         }
     }
 }

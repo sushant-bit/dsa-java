acbcd488a0984cae59c02a722bfa25bcbeba48fc
sushant-bit <sushantadhikari944@gmail.com>
2026-02-20 17:50:33 +0545

    Solve LeetCode 167 - Two Sum II

--- LeetCode/Random_Solving Question/Code/src/Probelm_167_TwoSum.java
+++ LeetCode/Random_Solving Question/Code/src/Probelm_167_TwoSum.java
@@ -1,4 +1,29 @@
+import java.util.Arrays;
+
 //167. Two Sum II - Input Array Is Sorted
 public class Probelm_167_TwoSum {
-    
+    public static void main(String[] args) {
+        int[] arr={1,2,3,7};
+        int [] res=twoSum(arr,46);
+        System.out.println(Arrays.toString(res));
+    }
+
+    static int[] twoSum(int[] numbers, int target) {
+        int start=0;
+        int end = numbers.length-1;
+
+        while (start<end){
+            int sum=numbers[start]+numbers[end];
+            if (sum==target){
+                return new int[] {start+1,end+1};
+            } else if (sum<target) {
+                start++;
+            }
+            else {
+                end--;
+            }
+        }
+
+        return new int[]{};
+    }
 }

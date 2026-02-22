0e374b41fe43e17c17db59f9570764874fa58833
sushant-bit <sushantadhikari944@gmail.com>
2026-02-22 20:10:23 +0545

    Solved Leetcode problem number 283
    Moving index which contain value zero at last index of an array.

--- LeetCode/Random_Solving Question/Code/src/Problem_283.java
+++ LeetCode/Random_Solving Question/Code/src/Problem_283.java
@@ -1,4 +1,26 @@
+import java.util.Arrays;
+
 //283. Move Zeroes
 public class Problem_283 {
+    public static void main(String[] args) {
+        int[] arr={1,0,2,3,23,0,12,0,2};
+        moveZero(arr);
+    }
 
+    static void moveZero(int[] arr){
+        int ind=0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]!=0){
+                arr[ind]=arr[i];
+                ind++;
+            }
+        }
+
+        while (ind< arr.length){
+            arr[ind]=0;
+            ind++;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
 }

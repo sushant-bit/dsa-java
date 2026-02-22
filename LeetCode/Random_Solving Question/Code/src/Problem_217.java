1c2fb983a89eab683314e85e0c83b268effba56f
sushant-bit <sushantadhikari944@gmail.com>
2026-02-22 19:07:41 +0545

    Solved Leetcode Problem Number 217 - Contains Duplicate

--- LeetCode/Random_Solving Question/Code/src/Problem_217.java
+++ LeetCode/Random_Solving Question/Code/src/Problem_217.java
@@ -1,3 +1,20 @@
 //217. Contains Duplicate
 public class Problem_217 {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4};
+        System.out.println(containsDuplicate(arr));
+    }
+
+    static boolean containsDuplicate(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j <arr.length ; j++) {
+                if(arr[i]==arr[j]){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+
+    }
 }

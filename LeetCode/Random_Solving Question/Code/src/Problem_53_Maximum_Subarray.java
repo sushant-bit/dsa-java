4a7bf0b20158fd5931b2da74dd596928d43e742d
sushant-bit <sushantadhikari944@gmail.com>
2026-02-22 19:48:26 +0545

    Solved Leetcode Problem Number 53

--- LeetCode/Random_Solving Question/Code/src/Problem_53_Maximum_Subarray.java
+++ LeetCode/Random_Solving Question/Code/src/Problem_53_Maximum_Subarray.java
@@ -1,4 +1,25 @@
 //53. Maximum Subarray
 public class Problem_53_Maximum_Subarray {
-    
+    public static void main(String[] args) {
+        int[] arr={10,5,-25,15,18};
+        System.out.println(maxSubarray(arr));
+    }
+
+    static int maxSubarray(int[]arr){
+        int newSum=arr[0];
+        int maxSum=arr[0];
+        for (int i = 1; i <arr.length ; i++) {
+            if (newSum<0){
+                newSum=arr[i];
+            }
+            else{
+                newSum+=arr[i];
+            }
+
+            if(newSum>maxSum){
+                maxSum=newSum;
+            }
+        }
+        return maxSum;
+    }
 }

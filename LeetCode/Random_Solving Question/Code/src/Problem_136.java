e68ebb740bc709fd2483cbf33f1f521b08854392
sushant-bit <sushantadhikari944@gmail.com>
2026-02-23 21:27:39 +0545

    Solved problem number 136

--- LeetCode/Random_Solving Question/Code/src/Problem_136.java
+++ LeetCode/Random_Solving Question/Code/src/Problem_136.java
@@ -1,2 +1,20 @@
 public class Problem_136 {
+    public static void main(String[] args) {
+
+    }
+
+    static int singleNumber(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int count=0;
+            for (int j = 0; j <nums.length ; j++) {
+                if (nums[i]==nums[j]){
+                    count++;
+                }
+                if(count==1){
+                    return nums[i];
+                }
+            }
+        }
+        return -1; 
+    }
 }

998936c0fadeb9f83318eca539d50149fdcbff18
sushant-bit <sushantadhikari944@gmail.com>
2026-02-23 20:57:34 +0545

    Solved Leetcode Problem Number 88

--- LeetCode/Random_Solving Question/Code/src/Problem_88_MergedArray.java
+++ LeetCode/Random_Solving Question/Code/src/Problem_88_MergedArray.java
@@ -1,4 +1,29 @@
 //88. Merge Sorted Array
 public class Problem_88_MergedArray {
+    public static void main(String[] args) {
+        
 
+    }
+
+    static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int lastInd = m + n - 1;
+
+        while (m > 0 && n > 0) {
+            if (nums1[m - 1] > nums2[n - 1]) {
+                nums1[lastInd] = nums1[m - 1];
+                m--;
+            } else {
+                nums1[lastInd] = nums2[n - 1];
+                n--;
+            }
+            lastInd--;
+        }
+
+        // Copy remaining nums2 elements in nums1
+        while (n > 0) {
+            nums1[lastInd] = nums2[n - 1];
+            n--;
+            lastInd--;
+        }
+    }
 }

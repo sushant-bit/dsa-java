b16e7c926ad8d58d5737e2196d8ff821dadc5da3
sushant-bit <sushantadhikari944@gmail.com>
2026-02-23 21:11:05 +0545

    Solved Question Number 268

--- LeetCode/Random_Solving Question/Code/src/Problem_268.java
+++ LeetCode/Random_Solving Question/Code/src/Problem_268.java
@@ -1,2 +1,17 @@
 public class Problem_268 {
+    public static void main(String[] args) {
+        int[] arr={1,2,0,4};
+        System.out.println(missingNumber(arr));
+    }
+
+    static int missingNumber(int[] nums) {
+        int n=nums.length;
+        int total_sum=n*(n+1)/2;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+
+        return total_sum-sum;
+    }
 }

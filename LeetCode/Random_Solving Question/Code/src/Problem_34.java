eaa824153b0c23011769a068c99a794588cdbfd4
sushant-bit <sushantadhikari944@gmail.com>
2026-02-25 20:02:46 +0545

    Finding first target

--- LeetCode/Random_Solving Question/Code/src/Problem_34.java
+++ LeetCode/Random_Solving Question/Code/src/Problem_34.java
@@ -1,5 +1,25 @@
 public class Problem_34{
     public static void main(String[] args) {
-        
+
+    }
+
+    static int firstTarget(int[] arr, int target){
+        int start=0;
+        int end= arr.length-1;
+        int ans=-1;
+
+        while (start<=end){
+            int mid=start+(end-start)/2;
+            if (arr[mid]==target){
+                ans=mid;
+                end=mid-1;
+            } else if (target>arr[mid]) {
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+
+        }
     }
 }

23cb51fe32a6f1ea3747adc4b7536a24598a2014
sushant-bit <sushantadhikari944@gmail.com>
2026-02-26 20:09:57 +0545

    Solved problem

--- LeetCode/Random_Solving Question/Code/src/Problem_134.java
+++ LeetCode/Random_Solving Question/Code/src/Problem_134.java
@@ -1,2 +1,35 @@
 public class Problem_134 {
+    public static void main(String[] args) {
+    
+    }
+
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+
+        int totalGas = 0;
+        int totalKharcha = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += gas[i];
+            totalKharcha += cost[i];
+        }
+
+        if (totalGas < totalKharcha) {
+            return -1;
+        }
+
+        //to find starting index.
+        int tank = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            tank += gas[i] - cost[i];
+
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+
+        return start;
+    }
 }

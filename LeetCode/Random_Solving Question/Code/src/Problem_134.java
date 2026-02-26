4bec5e8bd9c8e8483f54ceb7bdba609aaba7f60d
sushant-bit <sushantadhikari944@gmail.com>
2026-02-26 20:14:29 +0545

    Update Problem_134.java

--- LeetCode/Random_Solving Question/Code/src/Problem_134.java
+++ LeetCode/Random_Solving Question/Code/src/Problem_134.java
@@ -2,6 +2,8 @@ public class Problem_134 {
     public static void main(String[] args) {
 //     int[] arr={1,2,3,43};
 //     int[] cost={1,3}; // this input cause error length of cost and array should be equal
+        int[] arr = {1,2,3,4};
+        int[] cost = {1,3,2,4};
         System.out.println(canCompleteCircuit(arr,cost));
     }
 

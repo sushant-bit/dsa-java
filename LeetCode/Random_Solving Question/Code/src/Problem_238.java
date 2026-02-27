78ddf31a2de53da93aa5698455652e84a19504d5
sushant-bit <sushantadhikari944@gmail.com>
2026-02-27 20:40:56 +0545

    Update Problem_238.java

--- LeetCode/Random_Solving Question/Code/src/Problem_238.java
+++ LeetCode/Random_Solving Question/Code/src/Problem_238.java
@@ -1,4 +1,13 @@
 //238. Product of Array Except Self
 public class Problem_238 {
-    
+    public static void main(String[] args) {
+
+    }
+    public int[] productExceptSelf(int[] nums) {
+        int[] ans= new int[nums.length];
+        ans[0]=1;
+        for(int i=1;i<nums.length;i++){
+            ans[i]=answer[i-1]*nums[i-1];
+        }
+    }
 }

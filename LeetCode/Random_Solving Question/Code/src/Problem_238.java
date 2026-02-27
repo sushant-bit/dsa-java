f08de2d8fb2a75f9b835b45abd6f2946aec6df97
sushant-bit <sushantadhikari944@gmail.com>
2026-02-27 20:43:11 +0545

    Update Problem_238.java

--- LeetCode/Random_Solving Question/Code/src/Problem_238.java
+++ LeetCode/Random_Solving Question/Code/src/Problem_238.java
@@ -7,7 +7,13 @@ public class Problem_238 {
         int[] ans= new int[nums.length];
         ans[0]=1;
         for(int i=1;i<nums.length;i++){
-            ans[i]=answer[i-1]*nums[i-1];
+            ans[i]=ans[i-1]*nums[i-1];
+        }
+
+        int n=1;
+        for(int i=nums.length-1; i>=0;i--){
+            ans[i] = ans[i] * suffix;
+            n *= nums[i];
         }
     }
 }

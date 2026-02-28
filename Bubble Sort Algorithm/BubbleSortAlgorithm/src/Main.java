da43fedd0756a2800cacd53d95d1f60a1af2ac8e
sushant-bit <sushantadhikari944@gmail.com>
2026-02-28 21:45:48 +0545

    Update Main.java

--- Bubble Sort Algorithm/BubbleSortAlgorithm/src/Main.java
+++ Bubble Sort Algorithm/BubbleSortAlgorithm/src/Main.java
@@ -4,6 +4,12 @@ public class Main {
     }
 
     static void bubbleSort(int[]arr){
-        
+        //first run the steps n-1 times
+        for (int i = 0; i < arr.length; i++) {
+            //for each step max item will come last at the respective index
+            for (int j = 1; j < arr.length-i; j++) {
+
+            }
+        }
     }
 }

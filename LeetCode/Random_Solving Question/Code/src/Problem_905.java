70c3a5f36a861dff62987d3041275c9c9221f3df
sushant-bit <sushantadhikari944@gmail.com>
2026-02-28 22:08:06 +0545

    Update Problem_905.java

--- LeetCode/Random_Solving Question/Code/src/Problem_905.java
+++ LeetCode/Random_Solving Question/Code/src/Problem_905.java
@@ -1,11 +1,15 @@
+import java.util.Arrays;
+
 public class Problem_905 {
     public static void main(String[] args) {
-    
+    int[] arr={1,212,32,3,34};
+    sortArray(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     static void sortArray(int[]arr){
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 1; j < arr.length-i ; j++) {
+            for (int j = i+1; j < arr.length ; j++) {
                 if (arr[i] % 2 != 0 && arr[j] % 2 == 0) {
                     int temp = arr[i];
                     arr[i] = arr[j];

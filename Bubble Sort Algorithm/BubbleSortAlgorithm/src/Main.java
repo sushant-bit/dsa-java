0a29abf1956f07aa44e31011d82acb445d0e0065
sushant-bit <sushantadhikari944@gmail.com>
2026-02-28 21:48:55 +0545

    Swaping the value

--- Bubble Sort Algorithm/BubbleSortAlgorithm/src/Main.java
+++ Bubble Sort Algorithm/BubbleSortAlgorithm/src/Main.java
@@ -8,7 +8,13 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
             //for each step max item will come last at the respective index
             for (int j = 1; j < arr.length-i; j++) {
-
+            //swap if the item is smaller than the previous item
+                if(arr[j]<arr[j-1]){
+                    //swap the value
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
             }
         }
     }

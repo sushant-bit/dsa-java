fb89445d2be5c751b365f29e924d0ec6d0cfb801
sushant-bit <sushantadhikari944@gmail.com>
2026-02-28 22:06:06 +0545

    Solved problem number 905

--- LeetCode/Random_Solving Question/Code/src/Problem_905.java
+++ LeetCode/Random_Solving Question/Code/src/Problem_905.java
@@ -1,2 +1,17 @@
 public class Problem_905 {
+    public static void main(String[] args) {
+    
+    }
+
+    static void sortArray(int[]arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length-i ; j++) {
+                if (arr[i] % 2 != 0 && arr[j] % 2 == 0) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
 }

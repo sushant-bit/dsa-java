ffcaeeb27afbf6e110fdc8404090be3df4181b07
sushant-bit <sushantadhikari944@gmail.com>
2026-03-02 12:41:26 +0545

    Update Problem_912.java

--- LeetCode/Random_Solving Question/Code/src/Problem_912.java
+++ LeetCode/Random_Solving Question/Code/src/Problem_912.java
@@ -2,4 +2,16 @@ public class Problem_912 {
     public static void main(String[] args) {
 
     }
+    public int[] sortArray(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length-i; j++) {
+                if (nums[j]<nums[j-1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j - 1];
+                    nums[j - 1] = temp;
+                }
+            }
+        }
+        return nums;
+    }
 }

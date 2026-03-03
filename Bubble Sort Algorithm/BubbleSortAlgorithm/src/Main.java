daa7e36c028b0cf82da3baef83072b0c82d791a9
sushant-bit <sushantadhikari944@gmail.com>
2026-03-03 19:15:21 +0545

    Selection Sort Algorithm

--- Bubble Sort Algorithm/BubbleSortAlgorithm/src/Main.java
+++ Bubble Sort Algorithm/BubbleSortAlgorithm/src/Main.java
@@ -2,30 +2,30 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr={12,3,4,63,7,1};
+        int[] arr = {12, 3, 4, 63, 7, 1};
         bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    static void bubbleSort(int[]arr){
+    static void bubbleSort(int[] arr) {
         boolean swap;
         //first run the steps n-1 times
         for (int i = 0; i < arr.length; i++) {
             swap = false;
             //for each step max item will come last at the respective index
-            for (int j = 1; j < arr.length-i; j++) {
-            //swap if the item is smaller than the previous item
-                if(arr[j]<arr[j-1]){
+            for (int j = 1; j < arr.length - i; j++) {
+                //swap if the item is smaller than the previous item
+                if (arr[j] < arr[j - 1]) {
                     //swap the value
-                    int temp=arr[j];
-                    arr[j]=arr[j-1];
-                    arr[j-1]=temp;
-                    swap=true;
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swap = true;
                 }
             }
 
             //if swap isnot occured it means the array is sorted hence stop the program
-            if (!swap){
+            if (!swap) {
                 break;
             }
         }

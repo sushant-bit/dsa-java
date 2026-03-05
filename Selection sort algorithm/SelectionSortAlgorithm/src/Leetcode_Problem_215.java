98f9560b4f24bf0a2ab2203e7274acaef47e0389
sushant-bit <sushantadhikari944@gmail.com>
2026-03-05 17:24:05 +0545

    Problem Solved

--- Selection sort algorithm/SelectionSortAlgorithm/src/Leetcode_Problem_215.java
+++ Selection sort algorithm/SelectionSortAlgorithm/src/Leetcode_Problem_215.java
@@ -1,2 +1,25 @@
 public class Leetcode_Problem_215 {
+    public static void main(String[] args) {
+
+    }
+    public int findKthLargest(int[] nums, int k) {
+
+        for(int i = 0; i < k; i++) {
+
+            int maxIndex = i;
+
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[j] > nums[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+
+            int temp = nums[i];
+            nums[i] = nums[maxIndex];
+            nums[maxIndex] = temp;
+        }
+
+        return nums[k-1];
+    }
 }
+

fe031b2d07a1966a60f6447de1946ea0971c96a9
sushant-bit <sushantadhikari944@gmail.com>
2026-03-07 20:06:45 +0545

    Solved

--- LeetCode/Random_Solving Question/Code/src/Problem_1984.java
+++ LeetCode/Random_Solving Question/Code/src/Problem_1984.java
@@ -1,5 +1,44 @@
 public class Problem_1984 {
     public static void main(String[] args) {
-        
+
+    }
+    public int minimumDifference(int[] nums, int k) {
+
+        int n = nums.length;
+
+        if (k == 1) {
+            return 0;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            boolean adlabadli = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+
+                    adlabadli = true;
+                }
+            }
+
+            if (!adlabadli) {
+                break;
+            }
+        }
+
+        int minDiff = nums[k - 1] - nums[0];
+
+        for (int i = 1; i <= n - k; i++) {
+            int diff = nums[i + k - 1] - nums[i];
+
+            if (diff < minDiff) {
+                minDiff = diff;
+            }
+        }
+
+        return minDiff;
     }
 }

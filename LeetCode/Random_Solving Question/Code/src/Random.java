0ecbf3c74d083b9041fc61c0aa70976e04146be2
sushant-bit <sushantadhikari944@gmail.com>
2026-03-09 19:54:48 +0545

    Update Random.java

--- LeetCode/Random_Solving Question/Code/src/Random.java
+++ LeetCode/Random_Solving Question/Code/src/Random.java
@@ -3,8 +3,10 @@ import java.util.Arrays;
 public class Random {
     public static void main(String[] args) {
         int[] arr={1,24,6,0,4,9,0,4,3,0,5,3,0};
+        int[]arr2={1,2,3,4,5};
         moveAllZeroLast(arr);
         System.out.println(Arrays.toString(arr));
+        System.out.println(checkSorted(arr2));
     }
 
     static void moveAllZeroLast(int[]arr){
@@ -20,5 +22,16 @@ public class Random {
             arr[ind]=0;
             ind++;
         }
+    }
+
+    static boolean checkSorted(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }

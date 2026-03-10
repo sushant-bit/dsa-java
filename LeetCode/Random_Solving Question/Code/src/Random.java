9c679574a527e717a30174936e7e3f9a7ee660a3
sushant-bit <sushantadhikari944@gmail.com>
2026-03-10 20:27:43 +0545

    Add function to extract negative numbers and find average from array

--- LeetCode/Random_Solving Question/Code/src/Random.java
+++ LeetCode/Random_Solving Question/Code/src/Random.java
@@ -3,10 +3,12 @@ import java.util.Arrays;
 public class Random {
     public static void main(String[] args) {
         int[] arr={1,24,6,0,4,9,0,4,3,0,5,3,0};
-        int[]arr2={1,2,3,4,5};
+        int[]arr2={2,4,6,8,-2,-7};
         moveAllZeroLast(arr);
         System.out.println(Arrays.toString(arr));
         System.out.println(checkSorted(arr2));
+        System.out.println(average(arr2));
+        System.out.println(Arrays.toString(negativenum(arr2)));
     }
 
     static void moveAllZeroLast(int[]arr){
@@ -33,5 +35,33 @@ public class Random {
         }
 
         return true;
+    }
+
+    static int average(int[]arr){
+        int sum=0;
+        for (int i = 0; i < arr.length; i++) {
+            sum+=arr[i];
+        }
+        return sum/arr.length;
+    }
+
+    static int[] negativenum(int[]arr){
+        int count=0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]<0){
+                count++;
+            }
+        }
+
+        int[] negarr=new int[count];
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                negarr[index] = arr[i];
+                index++;
+            }
+        }
+
+        return negarr;
     }
 }

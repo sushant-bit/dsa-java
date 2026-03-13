054611c98e5b4f5c652520a6a636d57182d3abcf
sushant-bit <sushantadhikari944@gmail.com>
2026-03-13 20:54:42 +0545

    Update Random.java

--- LeetCode/Random_Solving Question/Code/src/Random.java
+++ LeetCode/Random_Solving Question/Code/src/Random.java
@@ -112,4 +112,16 @@ public class Random {
 
         return -1;
     }
+
+    static boolean isPerfect(int n) {
+        int sum = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum == n;
+    }
 }

62bd85eab727a3e3082cd93bf0b8da1a46304188
sushant-bit <sushantadhikari944@gmail.com>
2026-03-20 16:04:42 +0545

    Add brute-force solution for longest subarray with sum K

--- LeetCode/Random_Solving Question/Code/src/Random.java
+++ LeetCode/Random_Solving Question/Code/src/Random.java
@@ -148,4 +148,26 @@ public class Random {
             }
         }
 
+    static int longestSubarraySumK(int[] arr, int K) {
+        int maxLen = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+
+                if (sum == K) {
+                    int len = j - i + 1;
+                    if (len > maxLen) {
+                        maxLen = len;
+                    }
+                }
+            }
+        }
+
+        return maxLen;
+    }
+
+
 }

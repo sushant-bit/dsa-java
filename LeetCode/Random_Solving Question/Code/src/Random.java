bae56bd46aa7387eab987fb658ee0c6db6a5d0ba
sushant-bit <sushantadhikari944@gmail.com>
2026-03-21 21:49:16 +0545

    Update Random.java

--- LeetCode/Random_Solving Question/Code/src/Random.java
+++ LeetCode/Random_Solving Question/Code/src/Random.java
@@ -169,5 +169,16 @@ public class Random {
         return maxLen;
     }
 
+    
+    static int countNegatives(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
 
 }
